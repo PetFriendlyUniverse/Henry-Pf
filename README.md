0fc3c52e255808b13947837d8cb216abc893d7f3
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-02-10 15:24:20 -0300

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+Aca prueba  de readme
---
+++ api/src/routes/index.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+const router = Router();
+
+
+
+
+
+module.exports = router;
