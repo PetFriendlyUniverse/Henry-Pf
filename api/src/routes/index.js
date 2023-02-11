5e9bd9b4549a35040dd53c97cdca48a53e38a5c1
FedericoA15 <federicoalmeida15@gmail.com>
2023-02-11 12:01:25 -0300

    creacion de rutes en index y archivos

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,12 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 const router = Router();
 
-
-
-
+router.user("user", userRoutes);
+router.user("products", productsRoutes);
+router.user("invoices", invoicesRoutes);
+router.user("stores", storesRoutes);
 
 module.exports = router;
