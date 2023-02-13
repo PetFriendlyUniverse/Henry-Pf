e2a4e397de5c1ad01d3eb230ef852deb0a1b0d28
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-02-13 20:08:02 -0300

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">{/* <MiComponente /> */}</div>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
