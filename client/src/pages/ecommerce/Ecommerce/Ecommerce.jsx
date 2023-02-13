fafc566697be880391d8f4b57addd7ad83eb2cd2
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-02-13 11:02:16 -0300

    estructura basica de carpetas front

---
+++ client/src/pages/ecommerce/Ecommerce/Ecommerce.jsx
@@ -0,0 +1,29 @@
+// const producto = {
+//   qualification: null,
+//   enabled: true,
+//   id: 1,
+//   name: "Producto de Prueba",
+//   price: 15,
+//   description: "una descripción apropiada",
+//   stock: 30,
+//   specie: "pegeLag",
+//   breed: "volador",
+//   weight: null,
+//   color: null,
+//   size: null,
+//   updatedAt: "2023-02-12T22:39:51.400Z",
+//   createdAt: "2023-02-12T22:39:51.396Z",
+//   StoreId: 1,
+// };
+// const productos = [];
+// for (let i = 0; i < 20; i++) {
+//   productos.push({...producto, id:i});
+// }
+
+function Ecommerce() {
+  return <div>Ecommerce</div>;
+}
+
+export default Ecommerce;
+
+// rfce    <- para crear el componente (borrar import de React)
