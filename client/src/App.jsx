c2f3aba5c2e8f05f94aae193b83b5803bdf06fcb
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-02-13 19:58:20 -0300

    install react router-dom, brower router y una ruta para trabajar landing y shop

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { Routes, Route } from "react-router-dom";
+import Shop from "./pages/ecommerce/Shop/Shop";
+import Landing from "./pages/Landing/Landing";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">{/* <MiComponente /> */}</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
 // import store from "./redux/store"
 // import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <Provider store={store}>
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
   // </Provider>
 );
