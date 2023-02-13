e876161a0de7111d2443cda01e285d16de28f63c
FedericoA15 <federicoalmeida15@gmail.com>
2023-02-13 12:52:44 -0300

    favorites post, delete in progress

--- api/src/controllers/favoriteController.js
+++ api/src/controllers/favoriteController.js
@@ -1,11 +1,15 @@
 const { Product, User, Favorite } = require("../db");
 
+const searchFavorite = async ({ idUser, idProduct }) => {
+  const findFavorite = await Favorite.findByPk(idUser || idProduct);
+  return findFavorite;
+};
+
 const addFavoriteProduct = async ({ idUser, idProduct }) => {
   const user = await User.findByPk(idUser);
   if (!user) {
     throw new Error("User not found");
   }
-
   const product = await Product.findByPk(idProduct);
   if (!product) {
     throw new Error("Product not found");
@@ -18,6 +22,14 @@ const addFavoriteProduct = async ({ idUser, idProduct }) => {
   return favorite;
 };
 
+const deleteFavoriteById = async (idUser) => {
+  const favorite = await Favorite.findByPk(idUser);
+  await favorite.destroy();
+  return favorite;
+};
+
 module.exports = {
   addFavoriteProduct,
+  searchFavorite,
+  deleteFavoriteById,
 };
--- api/src/handler/favoriteHandler.js
+++ api/src/handler/favoriteHandler.js
@@ -1,10 +1,13 @@
 const {} = require("../controllers/commentsController");
-const { addFavoriteProduct } = require("../controllers/favoriteController");
+const {
+  addFavoriteProduct,
+  searchFavorite,
+  deleteFavoriteById,
+} = require("../controllers/favoriteController");
 
 const postFavorite = async (req, res) => {
   const { idUser, idProduct } = req.params;
   try {
-    console.log(idUser);
     const productAddFavorite = await addFavoriteProduct({ idUser, idProduct });
     return res.status(200).json(productAddFavorite);
   } catch (error) {
@@ -12,6 +15,33 @@ const postFavorite = async (req, res) => {
   }
 };
 
+const getFavorite = async (req, res) => {
+  const { idUser, idProduct } = req.params;
+  try {
+    let search;
+    if (idUser) {
+      search = await searchFavorite(idUser);
+    } else if (idProduct) {
+      search = await searchFavorite(idProduct);
+    }
+    return res.status(200).json(search);
+  } catch (error) {
+    return res.status(404).json(error.message);
+  }
+};
+
+const deleteFavorite = async (req, res) => {
+  const { idUser } = req.params;
+  try {
+    const favorite = await deleteFavoriteById(idUser);
+    res.status(200).json(favorite);
+  } catch (error) {
+    return res.status(404).json(error.message);
+  }
+};
+
 module.exports = {
   postFavorite,
+  getFavorite,
+  deleteFavorite,
 };
