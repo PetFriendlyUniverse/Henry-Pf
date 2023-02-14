bca114624c1ba245d82e761a962de93738dda4c8
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-02-13 21:11:50 -0300

    config redux-toolkit & getProducts(falta ruta)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-
-// import store from "./redux/store"
-// import { Provider } from "react-redux";
+import store from "./redux/store"
+import { Provider } from "react-redux";
+import axios from "axios"
+axios.defaults.baseURL = 'https://api.example.com';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // </Provider>
+  <Provider store={store}>
+  <App />
+  </Provider>
 );
--- client/src/redux/features/filterProducts/filterProductsSlice.js
+++ client/src/redux/features/filterProducts/filterProductsSlice.js
@@ -8,18 +8,12 @@ export const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
-    increment: (state) => {
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action) => {
+    getProducts: (state, action) => {
       state.value += action.payload;
     },
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { getProducts } = counterSlice.actions;
 
 export default counterSlice.reducer;
