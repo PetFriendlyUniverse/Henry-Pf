978a13571d6707d0c22964af19508bf92a8ad8b1
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-02-13 22:20:20 -0300

    config para peticiones desde front

--- api/src/app.js
+++ api/src/app.js
@@ -11,6 +11,17 @@ server.name = "API";
 server.use(morgan("dev"));
 
 server.use(express.json());
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 server.use("/", routes);
 
 // Error catching endware.
