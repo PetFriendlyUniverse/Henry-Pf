21b9a8a517fb16ccf85d82619f08382ee4bd1d7f
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-02-14 18:22:49 -0300

    install axios en el front npm install react-paginate

--- client/src/pages/ecommerce/Shop/Shop.jsx
+++ client/src/pages/ecommerce/Shop/Shop.jsx
@@ -1,15 +1,17 @@
-import { getProductsApi } from "../../../redux/features/filterProducts/filterProductsActions";
-import { useEffect } from "react";
+// import { getProductsApi } from "../../../redux/features/filterProducts/filterProductsActions";
+// import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Paginator from "@/components/Paginator/Paginator";
 
 export default function HomeShop() {
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
-  console.log(state.Products.products);
-  useEffect(() => {
-    dispatch(getProductsApi());
-  }, []);
+  // console.log(state.Products.products);
+
+  // useEffect(() => {
+  //   dispatch(getProductsApi());
+  // }, []);
+
   return (
     <div className="relative h-full w-full">
       <p>ACA ARRIBA VA LA NAV BAR</p>
