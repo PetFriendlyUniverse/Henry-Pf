2d8df348f58d2066d37b4b2d0bfb43e88daf572c
Benja <benyamin0714@gmail.com>
2023-02-15 14:35:03 -0300

    validacion de profile

---
+++ client/src/pages/ecommerce/Forms/FormProfile/Validations/Profile.js
@@ -0,0 +1,52 @@
+const validationProfile = (state) => {
+    let stringRegex = /^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/;
+    let numberRegex = /^[0-9]+$/;
+    let emailRegex =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    let passwordRegex = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
+  
+    //  /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i; ES EL ANTERIOR REGEX de mail
+  
+    let error = {};
+  
+    if (!state.name) {
+      error.name = "Please insert a name";
+    } else {
+      if (!stringRegex.test(state.name)) {
+        error.name = "The name cannot contain numbers or signs";
+      }
+    }
+  
+    if (!state.lastname) {
+      error.lastname = "Please insert a last name";
+    } else {
+      if (!stringRegex.test(state.lastname)) {
+        error.lastname = "The last name cannot contain numbers or signs";
+      }
+    }
+  
+    if (!state.phone) {
+      error.phone = "Please insert a last name";
+    } else {
+      if (!numberRegex.test(state.phone)) {
+        error.phone = "The last name cannot contain numbers or signs";
+      }
+    }
+  
+    if (state.mail) {
+      if (!emailRegex.test(state.mail)) {
+        errors.mail = "This not a valid mail";
+      } else {
+        errors.mail = "";
+      }
+    } else {
+      if (!passwordRegex.test(state.password)) {
+        errors.password = "Wrong password";
+      } else {
+        errors.password = "";
+      }
+    }
+  
+    return error;
+  };
+  
