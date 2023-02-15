e7d9a2304309cb22dd6065c677d8d3903c996fba
AgustinChavero <agustin199orellano@gmail.com>
2023-02-15 17:25:05 -0300

    aprueben y no jodan loco

--- client/src/components/Button/LinkButton.jsx
+++ client/src/components/Button/LinkButton.jsx
@@ -1,6 +1,6 @@
 function LinkButton({ component }) {
   return (
-    <button className="focus:bg-blue-900 mx-3 rounded-lg bg-blue px-3 py-2 text-center text-sm font-medium text-white shadow-md hover:bg-blueHover">
+    <button className="mx-3 rounded-lg bg-blue-900 px-3 py-2 text-center text-sm font-medium text-white shadow-md hover:bg-blueHover focus:bg-blue-900">
       {component}
     </button>
   );
