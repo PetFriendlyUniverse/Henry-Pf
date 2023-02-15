7a91b71fcc6aabc8a91b6487b6077a2eb2eb49d9
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-02-15 11:26:29 -0300

    cambios de formato de prettier

--- client/src/pages/ecommerce/Shop/Shop.jsx
+++ client/src/pages/ecommerce/Shop/Shop.jsx
@@ -16,13 +16,13 @@ export default function HomeShop() {
   return (
     <div className="relative h-full w-full">
       <p>ACA ARRIBA VA LA NAV BAR</p>
-      <div className="border-black absolute right-0 h-48 w-11/12 border-2">
+      <div className="absolute right-0 h-48 w-11/12 border-2 border-black">
         Banner de Recomendados
       </div>
-      <div className="border-black absolute -left-80 h-96 w-1/5 border-2">
+      <div className="absolute -left-80 h-96 w-1/5 border-2 border-black">
         filtros
       </div>
-      <div className="border-black absolute right-0 top-96 h-48 w-11/12 border-2">
+      <div className="absolute right-0 top-96 h-48 w-11/12 border-2 border-black">
         <Paginator />
         Paginator
       </div>
