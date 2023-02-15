63fdaa2294f4e5f39bde45c5402b31a7d695889a
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-02-14 21:54:42 -0300

    detalles de la navBar

--- client/src/pages/ecommerce/Shop/Shop.jsx
+++ client/src/pages/ecommerce/Shop/Shop.jsx
@@ -1,27 +1,27 @@
-// import { getProductsApi } from "../../../redux/features/filterProducts/filterProductsActions";
-// import { useEffect } from "react";
+import { getProductsApi } from "../../../redux/features/filterProducts/filterProductsActions";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Paginator from "@/components/Paginator/Paginator";
 
 export default function HomeShop() {
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
-  // console.log(state.Products.products);
+  console.log(state.Products.products);
 
-  // useEffect(() => {
-  //   dispatch(getProductsApi());
-  // }, []);
+  useEffect(() => {
+    dispatch(getProductsApi());
+  }, []);
 
   return (
     <div className="relative h-full w-full">
       <p>ACA ARRIBA VA LA NAV BAR</p>
-      <div className="absolute right-0 h-48 w-11/12 border-2 border-black">
+      <div className="border-black absolute right-0 h-48 w-11/12 border-2">
         Banner de Recomendados
       </div>
-      <div className="absolute -left-80 h-96 w-1/5 border-2 border-black">
+      <div className="border-black absolute -left-80 h-96 w-1/5 border-2">
         filtros
       </div>
-      <div className="absolute right-0 top-96 h-48 w-11/12 border-2 border-black">
+      <div className="border-black absolute right-0 top-96 h-48 w-11/12 border-2">
         <Paginator />
         Paginator
       </div>
