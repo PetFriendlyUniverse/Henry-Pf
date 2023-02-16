9cedd843cc6545f9111661a5eddb385b3a21df85
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-02-16 13:39:32 -0300

    queryMaker frontEnd primera parte

---
+++ client/src/redux/features/products/helpers/queryMaker.js
@@ -0,0 +1,23 @@
+const queryAdapter = {
+  name: "sr",
+  Breeds: "bd",
+  Brands: "bd",
+  Weigth: "w",
+  Species: "sp",
+  Size: "sz",
+  // c: "color",
+};
+
+// page: page, pq: productXpage, sortby, sortType; asc || des,    !! solo funciona si sortBy viene antes que sortType
+//minP: "minPrice" maxP: 'maxPrice'
+
+//  /products?pq=12&page=1
+
+const queryMarker = (productsPerPage, currentPage, setFilters) => {
+  let query = `?pq=${productsPerPage}&page=${currentPage}`;
+  for (let property in setFilters) {
+    query += `${queryAdapter[property]}${setFilters[property]}`;
+  }
+  return query;
+};
+export { queryMarker };
