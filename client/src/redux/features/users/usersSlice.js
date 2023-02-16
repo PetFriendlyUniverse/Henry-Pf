5d41f40bee523b392dd3980b99e23171e7bc7e31
AgustinChavero <agustin199orellano@gmail.com>
2023-02-16 09:50:20 -0300

    creacion de reducer de user pero sin utilizar

---
+++ client/src/redux/features/users/usersSlice.js
@@ -0,0 +1,20 @@
+//No sabemos si vamos a utilizar todo este codigo
+
+/* import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userID: null,
+};
+
+export const User = createSlice({
+  name: "User",
+  initialState,
+  reducers: {
+    getUser: (state, { payload }) => {
+      state.userID = payload;
+    },
+  },
+});
+
+export const { getUser } = User.actions;
+export default User.reducer; */
