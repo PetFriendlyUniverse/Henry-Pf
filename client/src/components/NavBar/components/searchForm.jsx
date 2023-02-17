9f54f3d8e3295cbeaf8acc69b577ae794513ef8f
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-02-17 20:20:48 -0300

    Search Bar
    funcionamiento de busqueda de la search bar

--- client/src/components/NavBar/components/searchForm.jsx
+++ client/src/components/NavBar/components/searchForm.jsx
@@ -1,13 +1,28 @@
 import search from "@/assets/search.svg";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setFilters } from "../../../redux/features/products/productsSlice";
 
 function searchForm() {
+  const [searchValue, setSearchValue] = useState("");
+  const dispatch = useDispatch();
+
+  const handleInputSearch = (event) => {
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    dispatch(setFilters({ filter: "name", value: searchValue }));
+  };
   return (
-    <form action="" className="relative flex">
+    <form action="" onSubmit={handleSearch} className="relative flex">
       <input
-        type="search"
-        name=""
-        id=""
+        type="text"
         className="w-full rounded-xl px-2 py-1"
+        value={searchValue}
+        onChange={handleInputSearch}
       />
       <img src={search} alt="" className="absolute right-0 top-1" />
     </form>
