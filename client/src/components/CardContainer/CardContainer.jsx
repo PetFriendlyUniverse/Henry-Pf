c3f19f0e06097994763c01361c4905d35c25ec79
Benja <benyamin0714@gmail.com>
2023-02-17 19:14:33 -0300

    si no encuentra productos... y tarea de setear en 1 la currentPage

--- client/src/components/CardContainer/CardContainer.jsx
+++ client/src/components/CardContainer/CardContainer.jsx
@@ -6,19 +6,23 @@ function CardContainer() {
 
   return (
     <div className="flex w-full flex-wrap justify-center gap-2 border-2 border-black">
-      {products.map((product) => {
-        return (
-          <Card
-            key={product.id}
-            id={product.id}
-            name={product.name}
-            img={product.img}
-            weight={product.weight}
-            price={product.price}
-            stock={product.stock}
-          />
-        );
-      })}
+      {products?.length === 0 ? (
+        <h2>No se encotraron coincidencias con los datos solicitados</h2>
+      ) : (
+        products?.map((product) => {
+          return (
+            <Card
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              img={product.img}
+              weight={product.weight}
+              price={product.price}
+              stock={product.stock}
+            />
+          );
+        })
+      )}
     </div>
   );
 }
