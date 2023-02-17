b609a9bfe5bcab8347d10a2f8f9f6e658770d200
Benja <benyamin0714@gmail.com>
2023-02-17 15:26:09 -0300

    Estilos paginator (no finales)

--- client/src/components/Paginator/Paginator.jsx
+++ client/src/components/Paginator/Paginator.jsx
@@ -4,6 +4,8 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductsApi } from "../../redux/features/products/productsActions";
 import { setCurrentPage } from "../../redux/features/products/productsSlice";
+import arrowLeft from "../../assets/arrows/dobleArrowToLeft.svg";
+import arrowRight from "../../assets/arrows/dobleArrowToRight.svg";
 
 function Paginator() {
   let { totalPages, productsPerPage, currentPage, setFilters } = useSelector(
@@ -20,20 +22,29 @@ function Paginator() {
   };
 
   return (
-    <div className="flex h-full w-full flex-col">
-      <Paginate
-        className="flex justify-center border-2 border-black"
-        previousLabel={"previous"}
-        nextLabel={"next"}
-        pageRangeDisplayed={3}
-        pageCount={totalPages}
-        onPageChange={changePage}
-        containerClassName={"paginationButtons"}
-        previousLinkClassName={"pr-8"}
-        nextLinkClassName={"pl-8"}
-        disabledClassName={"paginationDisabled"}
-        activeClassName={"text-xl"}
-      />
+    <div className="src flex h-full w-full flex-col bg-slate-500  ">
+      <div className="w-2/4 self-center">
+        <Paginate
+          containerClassName={
+            "flex text-xl h-16 font-mono w-full justify-between items-center border-2 border-black "
+          }
+          previousLabel={
+            // prettier-ignore
+            <img className="mt-1 mr-3 w-5" src={arrowLeft} alt="arrowLeft" />
+          }
+          nextLabel={
+            // prettier-ignore
+            <img className="ml-3 mt-1 w-5 " src={arrowRight} alt="arrowRight" />
+          }
+          pageRangeDisplayed={3}
+          pageCount={totalPages}
+          onPageChange={changePage}
+          previousLinkClassName={""}
+          nextLinkClassName={""}
+          disabledClassName={"paginationDisabled"}
+          activeClassName={"text-4xl px-3 font-extrabold"}
+        />
+      </div>
       <CardContainer className="w-full" />
     </div>
   );
