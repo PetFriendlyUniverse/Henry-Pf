cbd3599736bbf4d4ac431e1fb4a8d2c8de1f6af6
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-02-19 17:56:54 -0300

    renderizado de shopcart en checkout, redireccion al finalizar compra, scroll modalshop

--- client/src/components/Button/LinkButton.jsx
+++ client/src/components/Button/LinkButton.jsx
@@ -1,6 +1,6 @@
 function LinkButton({ component }) {
   return (
-    <button className="mx-3 rounded-lg bg-blue-900 px-3 py-2 text-center text-sm font-medium text-white shadow-md hover:bg-blueHover">
+    <button className="mx-3 rounded-md bg-blue-900 px-3 py-2 text-center text-sm font-medium text-white shadow-md hover:bg-blueHover">
       {component}
     </button>
   );
--- client/src/pages/ecommerce/Checkout/Checkout.jsx
+++ client/src/pages/ecommerce/Checkout/Checkout.jsx
@@ -1,14 +1,32 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import Subcard from "../../../components/SubCard/Subcard";
 import { clearShopCart } from "../../../redux/features/products/productsSlice";
 
 function Checkout() {
+  const navigate = useNavigate();
+  const shopCartProducts = useSelector((state) => state.Products?.shopCart);
+  const productsIds = Object.keys(shopCartProducts);
+  let totalPrice = 0;
+  const products = productsIds.map((id) => {
+    totalPrice += shopCartProducts[id].amount * shopCartProducts[id].price;
+    return shopCartProducts[id];
+  });
   const dispatch = useDispatch();
-  const handleClick = () => {
+  const handleClick = async () => {
+    await Swal.fire({
+      icon: "success",
+      title: "Tu compra ha sido realizada con éxito!",
+      showConfirmButton: false,
+      timer: 1100,
+    });
     localStorage.removeItem("shopCart");
     dispatch(clearShopCart());
+    navigate("/shop");
   };
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-slate-200 pt-20">
+    <div className="flex h-screen w-full items-center justify-center bg-slate-200 pt-24">
       <div className="my-4 flex h-[80vh] w-full flex-col items-center justify-center gap-8 bg-slate-200 lg:w-[60vw] lg:flex-row lg:gap-0">
         <div className="flex h-full w-full flex-col rounded-lg border  bg-white lg:m-4 lg:w-4/6 lg:items-center lg:justify-center lg:px-6 ">
           <div className="m-6 hidden h-[8%] w-full items-center justify-center rounded-lg border bg-blue-200 px-6 text-center text-xs  text-gray-600  lg:flex">
@@ -16,16 +34,26 @@ function Checkout() {
             the majority have suffered alteration in some form, by injected
             humour
           </div>
-          <div className=" flex h-3/6 w-full  items-center justify-center border">
-            aca van los componentes de los productos comprados
+
+          <div
+            className={` flex h-3/6 w-full flex-col items-center justify-center gap-2  ${
+              products.length && "overflow-scroll"
+            } overflow-x-hidden`}
+          >
+            <div className="h-full w-full">
+              {products?.map((prod) => (
+                <Subcard prod={prod} key={prod.id} />
+              ))}
+            </div>
           </div>
+
           <div className=" flex h-2/6 w-full items-center justify-center border">
             aca van las cards de los productos
           </div>
         </div>
         <div className=" flex h-4/6 w-full flex-col self-start lg:w-2/6 ">
-          <div className="flex h-1/6 w-full items-center  justify-center rounded-lg bg-blue-200 text-lg  text-blue-700 lg:m-4">
-            Tenes un cupon de descuento??
+          <div className="flex h-1/6 w-full items-center  justify-center rounded-lg bg-blue-200 text-lg  text-blue-700 lg:p-4">
+            <span className="font-semibold">¿Tenes un cupón de decuento?</span>
           </div>
           <div className="flex h-5/6 w-full flex-col items-center justify-between p-2  lg:m-4">
             <h1 className=" self-start text-3xl font-bold  text-gray-900">
@@ -39,13 +67,21 @@ function Checkout() {
             </span>
             <hr className="my-2 h-[0.10rem] w-full border-0 bg-gray-400" />
             <div className="flex w-full justify-between">
-              <span className="text-gray-500">subtotal</span>
-              <span className="text-gray-500">1000.00</span>
+              <span className="font-semibold uppercase tracking-widest text-gray-500">
+                Subtotal
+              </span>
+              <span className="font-semibold tracking-wider text-gray-500">
+                ${totalPrice}
+              </span>
             </div>
             <hr className="my-2 h-[0.10rem] w-full border-0 bg-gray-400" />
             <div className="flex w-full  justify-between text-lg">
-              <h3 className=" font-bold text-gray-900">Total</h3>
-              <h3 className=" font-bold  text-blue-700">1000.00</h3>
+              <h3 className=" font-bold uppercase tracking-widest text-gray-900">
+                Total
+              </h3>
+              <h3 className="font-semibold tracking-wider  text-blue-700">
+                ${totalPrice}
+              </h3>
             </div>
 
             <button
