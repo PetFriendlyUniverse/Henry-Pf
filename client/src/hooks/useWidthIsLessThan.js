9d45f9dcb5265220f7782111e341bd6ad2681b5f
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-02-19 17:54:37 -0300

    rename useIsMobile => useWidthIsLessThan

--- client/src/hooks/useWidthIsLessThan.js
+++ client/src/hooks/useWidthIsLessThan.js
@@ -1,18 +1,18 @@
 import { useState, useEffect } from "react";
 
-function useIsMobile(breakpoint) {
-  const [mobile, setMobile] = useState(true);
+function useWidthIsLessThan(breakpoint) {
+  const [lessThan, setLessThan] = useState(true);
 
   const resizeListener = (event) => {
-    event.target.innerWidth > breakpoint && setMobile(false);
-    event.target.innerWidth < breakpoint && setMobile(true);
+    event.target.innerWidth >= breakpoint && setLessThan(false);
+    event.target.innerWidth < breakpoint && setLessThan(true);
   };
   useEffect(() => {
-    window.innerWidth > breakpoint && setMobile(false); // esto es necesario para ejecutarlo la primera vez que entramos a la pag
+    window.innerWidth >= breakpoint && setLessThan(false); // esto es necesario para ejecutarlo la primera vez que entramos a la pag
     window.addEventListener("resize", resizeListener); // agrega la escucha al resize ( cambio de ancho de pantalla ) para que actualice el estado en caso de ser necesario
     return () => window.removeEventListener("resize", resizeListener); // importantísimo! esto remueve la escucha para no mantenerla en las demas paginas donde no se necesita
   }, []);
-  return mobile;
+  return lessThan;
 }
 
-export default useIsMobile;
+export default useWidthIsLessThan;
