7fff6436199f88398c00e7645abb0ccb6ebfca9e
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-02-20 11:11:20 -0300

    armando controller, handler y ruta para BRANDS y poder traer al fron las marcas para renderizar en el banner de recomendados

--- client/src/pages/ecommerce/Shop/Shop.jsx
+++ client/src/pages/ecommerce/Shop/Shop.jsx
@@ -1,13 +1,12 @@
 import Paginator from "../../../components/Paginator/Paginator";
 import Filters from "./components/Filters/Filters";
+import Recomendados from "../../../components/Recomendados/Recomendados";
 
-export default function HomeShop() {
+export default function HomeShop({ brands }) {
   return (
     <div className="scrollbar scrollbar-rounded-xl scrollbar-thumb-blue-500 scrollbar-track-blue-300  w-full lg:flex lg:min-h-screen lg:flex-col lg:gap-12 lg:pt-20 xl:items-center">
-      <div className=" hidden border-2 border-black pt-20 md:block md:h-44  lg:h-28 lg:w-full">
-        Banner de Recomendados
-      </div>
-
+      <div className="flex border-2 border-black pt-20 md:block md:h-44  lg:h-28 lg:w-full"></div>
+      <Recomendados></Recomendados>
       <div className="lg:flex lg:h-full lg:justify-between xl:max-w-[1800px]  ">
         <div className="lg:border-blue lg:border-#645e9d ml-2 h-fit w-full border-2 lg:sticky lg:top-16 lg:w-1/5 lg:border-2">
           <Filters />
