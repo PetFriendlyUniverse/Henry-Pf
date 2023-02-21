64f0d1ad0d2461238cd73af4152f5d1b92e7e049
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-02-21 10:51:02 -0300

    estilado perfil navbar y algunos links de la landingshop

--- client/src/components/NavBar/components/DropdownUser.jsx
+++ client/src/components/NavBar/components/DropdownUser.jsx
@@ -1,12 +1,13 @@
 import { useRef } from "react";
 import { Link } from "react-router-dom";
+import user from "../../../assets/general/profile.svg";
 
 function DropdownUser() {
   return (
     <div className="group relative flex items-center gap-2 md:order-2 lg:flex-col">
       <button
         type="button"
-        className="mr-3 flex rounded-full bg-gray-800 text-sm focus:ring-4 focus:ring-gray-300  md:mr-0"
+        className="mr-3 flex rounded-full  text-sm focus:ring-4 focus:ring-gray-300  md:mr-0"
         id="user-menu-button"
         aria-expanded="false"
         data-dropdown-toggle="user-dropdown"
@@ -15,47 +16,40 @@ function DropdownUser() {
         <span className="sr-only">Open user menu</span>
         <img
           className="h-10 rounded-full sm:h-12 md:h-12 lg:h-12"
-          src="https://pbs.twimg.com/profile_images/1584012412272971776/ngjy-YE8_400x400.jpg"
+          src={user}
           alt="user photo"
         />
       </button>
       <div
-        className="absolute top-7 -left-20 z-50 my-4 hidden list-none divide-y divide-gray-100 rounded-lg bg-primary text-base text-lightwhite  shadow group-hover:block  lg:-left-12"
+        className="absolute top-8 -left-20 z-50 my-4 hidden list-none divide-y divide-gray-100 rounded-lg bg-blue-600 p-2 text-base text-lightwhite  shadow group-hover:block  lg:-left-12"
         id="user-dropdown"
       >
         <div className="px-4 py-3">
           <span className="block text-sm text-gray-900 dark:text-white">
-            Bonnie Green
+            Federico Almeida
           </span>
           <span className="block truncate text-sm font-medium text-gray-500 dark:text-gray-400">
-            name@flowbite.com
+            federicoalmeida15@gmail.com
           </span>
         </div>
         <ul className="py-2" aria-labelledby="user-menu-button">
           <li>
-            <a
-              href="#"
+            <Link
+              to="/login"
               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
             >
-              Dashboard
-            </a>
+              Login
+            </Link>
           </li>
           <li>
             <Link
-              to={`profile/1`}
+              to="profile/1"
               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
             >
               Settings
             </Link>
           </li>
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Earnings
-            </a>
-          </li>
+
           <li>
             <a
               href="#"
