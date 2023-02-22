7c328040220857abded1867a1e7936e03c95115c
AgustinChavero <agustin199orellano@gmail.com>
2023-02-22 15:31:35 -0300

    estructura formulario de store

---
+++ client/src/pages/ecommerce/Forms/FormCreateStore/FormCreateStore.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import LinkButton from "../../../../components/Button/LinkButton";
+
+function FormCreateStore() {
+  const [form, setForm] = useState({
+    name: "",
+    phone: "",
+    province: "",
+    locality: "",
+    streets: "",
+    description: "",
+  });
+  const [errors, setErrors] = useState({
+    name: "",
+    phone: "",
+    province: "",
+    locality: "",
+    streets: "",
+    description: "",
+  });
+  const handleChange = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="flex h-screen justify-center py-28">
+      <form
+        onSubmit={handleSubmit}
+        className="rounded-xl p-3 shadow-2xl lg:h-[550px] lg:w-2/5"
+      >
+        <h3 className="mb-6">Registra tu Tienda</h3>
+        <div className="group relative z-0 mb-6 h-11 w-full">
+          <input
+            onChange={handleChange}
+            type="text"
+            name="name"
+            value={form.name}
+            className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900  focus:outline-none focus:ring-0 dark:border-gray-600 dark:focus:border-gray-900 "
+            placeholder=" "
+            autoComplete="off"
+          />
+          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-gray-900 dark:text-gray-400 peer-focus:dark:text-gray-900">
+            Nombre de la Tienda (max: 30 caracteres)
+          </label>
+        </div>
+        <div className="group relative z-0 mb-6 h-11 w-full">
+          <select class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
+            <option hidden>Selecciona una Provincia</option>
+            <option value="bsas">Buenos Aires</option>
+          </select>
+        </div>
+        <div className="group relative z-0 mb-6 h-11 w-full">
+          <select class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
+            <option hidden>Selecciona una Localidad</option>
+            <option value="bsas">Mar del Plata</option>
+          </select>
+        </div>
+        <div className="group relative z-0 mb-6 h-11 w-full">
+          <input
+            onChange={handleChange}
+            type="text"
+            name="streets"
+            value={form.streets}
+            className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900  focus:outline-none focus:ring-0 dark:border-gray-600 dark:focus:border-gray-900 "
+            placeholder=" "
+            autoComplete="off"
+          />
+          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-gray-900 dark:text-gray-400 peer-focus:dark:text-gray-900">
+            Direccion (max: 100 caracteres)
+          </label>
+        </div>
+        <div className="group relative z-0 mb-6 h-11 w-full">
+          <input
+            onChange={handleChange}
+            type="number"
+            name="phone"
+            value={form.phone}
+            className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600 dark:focus:border-gray-900 "
+            placeholder=" "
+            autoComplete="off"
+          />
+          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-gray-900 dark:text-gray-400 peer-focus:dark:text-gray-900">
+            Telefono (max: 20 caracteres)
+          </label>
+        </div>
+        <div className="group relative z-0 mb-6 h-11 w-full">
+          <input
+            onChange={handleChange}
+            type="text"
+            name="description"
+            value={form.description}
+            className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600 dark:focus:border-gray-900 "
+            placeholder=" "
+            autoComplete="off"
+          />
+          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-gray-900 dark:text-gray-400 peer-focus:dark:text-gray-900">
+            Descripcion (max: 2000 caracteres)
+          </label>
+        </div>
+        <LinkButton component={"Enviar"} />
+      </form>
+    </div>
+  );
+}
+
+export default FormCreateStore;
