84a1d466c09e7303223db8f954751a23ba1b09d1
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-02-22 21:03:12 -0300

    prueba5000

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+require("dotenv").config();
+
+const port = process.env.port || 3001;
 
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
-    console.log('%s listening at 3001');
+    console.log("%s listening at 3001");
   });
 });
