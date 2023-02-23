103c4187ec6444cbb8bd91fe9af5594ec6c8b4f6
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-02-23 09:34:49 -0300

    push para deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
 import axios from "axios";
-// axios.defaults.baseURL = process.env.BASE_URL || "http://localhost:3001";
+// axios.defaults.baseURL = process.env.BASE_URL || "http://localhost:3001"; pushparadeploy
 axios.defaults.baseURL = "https://petfriendly-backend.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
