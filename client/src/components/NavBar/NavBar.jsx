1a77532e2a67ef7f85e8d1b475b9a99ae7d64051
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-02-23 20:49:58 -0300

    navbar nueva

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,150 +1,286 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import SearchForm from "./components/SearchForm";
-import DropdownHelp from "./components/DropdownHelp";
-import DropdownShop from "./components/DropdownShop";
 import logo from "../../assets/logo/logo.png";
-import services from "../../assets/general/services.svg";
-import shop from "../../assets/general/shop.svg";
-import DropdownUser from "./components/DropdownUser";
-function NavBar() {
-  const token = localStorage.getItem("token");
 
-  const toolBar = useRef(null);
-  const toggleBar = () => {
-    toolBar.current.classList.toggle("hidden");
+import shop from "../../assets/general/shop.svg";
+import services from "../../assets/general/services.svg";
+import about from "../../assets/general/about.svg";
+import user from "../../assets/general/profile.svg";
+import arrowDropdown from "../../assets/arrows/arrowDropdown.svg";
+import shopcart from "../../assets/general/shopcart.svg";
+import { useDispatch, useSelector } from "react-redux";
+import Swal from "sweetalert2";
+import { clearShopCart } from "../../redux/features/products/productsSlice";
+import deleteBtn from "../../assets/general/delete.svg";
+import LinkButton from "../Button/LinkButton";
+import Subcard from "../SubCard/Subcard";
+import SearchForm from "./components/SearchForm";
+import DropdownUser from "./components/DropdownUser";
+import login from "../../assets/general/login.svg";
+
+function NavBarPrueba() {
+  // Logica de usuario
+  const token = localStorage.getItem("token");
+  // -------------------------------------
+  const [active, setActive] = useState(false);
+  // ---------------------------
+  const menu = useRef(null);
+  const toggleSvgBars = () => {
+    setActive(!active);
+    menu.current.classList.toggle("hidden");
+  };
+  // ----------------------------
+  const userMenu = useRef(null);
+  const toggleUserMenu = () => {
+    userMenu.current.classList.toggle("hidden");
+    arrow.current.classList.toggle("rotate-180");
+  };
+  // ----------------------------
+  const arrow = useRef(null);
+  let activeClassName = "border-b border-white ";
+  // --------------- Logic Shop Modal -------------------- //
+  const dispatch = useDispatch();
+  const shopCartProducts = useSelector((state) => state.Products?.shopCart);
+  const productsIds = Object.keys(shopCartProducts);
+  let totalPrice = 0;
+  const products = productsIds.map((id) => {
+    totalPrice += shopCartProducts[id].amount * shopCartProducts[id].price;
+    return shopCartProducts[id];
+  });
+  const handleDelete = () => {
+    totalPrice > 0 &&
+      Swal.fire({
+        icon: "warning",
+        title: "Está seguro de que quiere eliminar su carro de compras?",
+        showConfirmButton: true,
+        confirmButtonText: "Si",
+        showDenyButton: true,
+        denyButtonText: "No",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          dispatch(dispatch(clearShopCart()));
+          Swal.fire({
+            title: "Productos Eliminados",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+  const modalShop = useRef(null);
+  const img = useRef(null);
+  const handleModalShow = () => {
+    modalShop.current.classList.toggle("hidden");
+    img.current.classList.toggle("rotate-180");
   };
   return (
-    <nav className="fixed z-30 flex h-24 w-full bg-russianviolet sm:px-4">
-      <div className="flex w-full items-center justify-around sm:justify-evenly md:justify-between lg:justify-between">
-        <Link to={`/`}>
+    <nav className="sticky top-0  z-50 flex w-full flex-col items-center justify-between bg-russianviolet p-4 2xl:py-5">
+      {/* Contenedor flex logo y button mobile */}
+      <div className="flex w-full items-center justify-between    pb-2">
+        {/* Logo */}
+        <Link to="/">
           <img
             src={logo}
-            className="mr-3 h-10 sm:h-12 md:h-14 lg:h-16 xl:h-20"
-            alt="PetShopUniverse Logo"
+            alt=""
+            className="h-10 w-10 sm:h-12 sm:w-12 md:h-14 md:w-14 lg:h-16 lg:w-16"
           />
         </Link>
-
-        <div className="flex items-center gap-10">
-          <NavLink
-            className=" flex gap-5 rounded-md border border-cornflowerblue py-2 px-4 text-base font-semibold text-cornflowerblue shadow-sm shadow-cornflowerblue active:shadow-inner active:shadow-cornflowerblue"
-            to="/landingshop"
-          >
-            <img src={shop} alt="" />
-            Shop
-          </NavLink>
-          <div>
-            <NavLink
-              className="flex gap-5 rounded-md border border-cornflowerblue py-2 px-4 text-base font-semibold text-cornflowerblue shadow-sm shadow-cornflowerblue
-               active:shadow-inner active:shadow-cornflowerblue"
-              to="/services"
+        {/* Button bars mobile */}
+        <div className="block lg:hidden">
+          <button onClick={toggleSvgBars}>
+            {!active ? (
+              <svg
+                fill="#ffffff"
+                height="20px"
+                width="20px"
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 1792 1792"
+                xmlSpace="preserve"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <title>fiction</title>{" "}
+                  <path d="M1673.9,1363.2L1673.9,1363.2c0,52.3-42.4,94.3-94.3,94.3H212.7c-52.3,0-94.3-42.4-94.3-94.3l0,0 c0-52.3,42.4-94.3,94.3-94.3h1366.8C1631.5,1268.5,1673.9,1310.9,1673.9,1363.2z"></path>{" "}
+                  <path d="M1673.9,895.6L1673.9,895.6c0,52.3-42.4,94.3-94.3,94.3H213c-52.3,0-94.3-42.4-94.3-94.3l0,0c0-52.3,42.4-94.3,94.3-94.3 h1366.6C1631.5,800.8,1673.9,843.2,1673.9,895.6z"></path>{" "}
+                  <path d="M1673.9,427.9L1673.9,427.9c0,52.3-42.4,94.3-94.3,94.3H212.7c-52.3,0-94.3-42.4-94.3-94.3l0,0c0-52.3,42.4-94.3,94.3-94.3 h1366.8C1631.5,333.2,1673.9,375.6,1673.9,427.9z"></path>{" "}
+                </g>
+              </svg>
+            ) : (
+              <svg
+                fill="#ffffff"
+                height="20px"
+                width="20px"
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 1792 1792"
+                xmlSpace="preserve"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <title>fiction</title>{" "}
+                  <path d="M1673.9,1363.2L1673.9,1363.2c0,52.3-42.4,94.3-94.3,94.3H212.7c-52.3,0-94.3-42.4-94.3-94.3l0,0 c0-52.3,42.4-94.3,94.3-94.3h1366.8C1631.5,1268.5,1673.9,1310.9,1673.9,1363.2z"></path>{" "}
+                  <path d="M1673.9,895.6L1673.9,895.6c0,52.3-42.4,94.3-94.3,94.3H659c-52.3,0-94.3-42.4-94.3-94.3l0,0c0-52.3,42.4-94.3,94.3-94.3 h920.6C1631.5,800.8,1673.9,843.2,1673.9,895.6z"></path>{" "}
+                  <path d="M1673.9,427.9L1673.9,427.9c0,52.3-42.4,94.3-94.3,94.3H212.7c-52.3,0-94.3-42.4-94.3-94.3l0,0c0-52.3,42.4-94.3,94.3-94.3 h1366.8C1631.5,333.2,1673.9,375.6,1673.9,427.9z"></path>{" "}
+                </g>
+              </svg>
+            )}
+          </button>
+        </div>
+      </div>
+      {/* Contenedor links ([ "shop", "services", "aboutUs", "userMenu"]) */}
+      <div
+        ref={menu}
+        className=" hidden  w-full pt-5 lg:absolute  lg:right-0  lg:top-7 lg:flex  lg:w-[900px] lg:items-center lg:gap-3  lg:p-0 lg:pt-0   2xl:top-6 2xl:right-0   2xl:w-[750px]"
+      >
+        {/* link to landing shop */}
+        <NavLink
+          to="/shop"
+          className="flex w-full gap-3 py-3 text-cornflowerblue "
+        >
+          {({ isActive }) => (
+            <div
+              className={`${
+                isActive ? activeClassName : undefined
+              } flex w-full  gap-3 pb-1 lg:justify-center`}
+            >
+              <img src={shop} alt="" />
+              <span className="font-bold uppercase">shop</span>
+            </div>
+          )}
+        </NavLink>
+        {/* link to services */}
+        <NavLink
+          to="/services"
+          className="flex w-full gap-3 py-3 text-cornflowerblue "
+        >
+          {({ isActive }) => (
+            <div
+              className={`${
+                isActive ? activeClassName : undefined
+              } flex w-full gap-3 pb-1 lg:justify-center`}
             >
               <img src={services} alt="" />
-              Servicios
-            </NavLink>
-          </div>
-        </div>
-
-        <button
-          data-collapse-toggle="mobile-menu-2"
-          type="button"
-          className="ml-1 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-amber-900 focus:outline-none focus:ring-2 focus:ring-gray-200    lg:hidden"
-          aria-controls="mobile-menu-2"
-          aria-expanded="false"
-          onClick={toggleBar}
+              <span className="font-bold uppercase">services</span>
+            </div>
+          )}
+        </NavLink>
+        {/* Link to about */}
+        <NavLink
+          to="/about"
+          className="flex w-full gap-3 py-3 text-cornflowerblue  "
         >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="h-6 w-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+          {({ isActive }) => (
+            <div
+              className={`${
+                isActive ? activeClassName : undefined
+              } flex w-full gap-3 pb-1 lg:justify-center lg:gap-2 xl:gap-3`}
+            >
+              <img src={about} alt="" />
+              <span className=" font-bold uppercase">sobre nosotros</span>
+            </div>
+          )}
+        </NavLink>
+        {/* dropdown menu User */}
+        {token ? (
+          <DropdownUser />
+        ) : (
+          <NavLink
+            className="flex w-full justify-center gap-3 py-3 text-cornflowerblue  "
+            to={"/login"}
           >
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </button>
-        <div className="w-96 ">
-          <SearchForm />
-        </div>
-        <div
-          className=" absolute top-14 left-0 z-10 hidden  items-center justify-between  md:order-1 lg:static lg:flex "
-          id="mobile-menu-2"
-          ref={toolBar}
-        >
-          <ul className="mt-4 flex flex-col items-center rounded-lg border border-gray-100 md:mt-0 md:flex-row md:justify-between md:space-x-8 md:border-0  md:text-sm md:font-medium lg:px-10 ">
-            <li>
-              <DropdownHelp />
-            </li>
-            <li>
-              <DropdownShop />
-            </li>
-            <li className="rounded-md border border-cornflowerblue py-2 px-4 text-base text-cornflowerblue shadow-sm shadow-cornflowerblue active:shadow-inner active:shadow-cornflowerblue">
-              <Link to="/about">Sobre Nosotros</Link>
-            </li>
-            <li>
-              {token ? <DropdownUser /> : <Link to={"/login"}>Login</Link>}
-            </li>
-          </ul>
+            {({ isActive }) => (
+              <div
+                className={`${
+                  isActive ? activeClassName : undefined
+                } flex w-full gap-3 pb-1 lg:justify-center lg:gap-2 xl:gap-3`}
+              >
+                <img src={login} alt="" />
+                <span className=" font-bold uppercase">login</span>
+              </div>
+            )}
+          </NavLink>
+        )}
+      </div>
+
+      {/* contenedor serchBar and modalShop */}
+      <div className="xl:w- flex w-full flex-col items-center justify-center gap-4 sm:w-[500px] md:w-full md:flex-row md:justify-end lg:h-16 2xl:absolute 2xl:left-36  2xl:top-4 2xl:w-[800px] 2xl:flex-row-reverse ">
+        {/* searchBar  */}
+        <SearchForm />
+
+        {/* ShopModal */}
+        <div className="flex w-full flex-col items-center  md:relative md:mt-2 md:w-52">
+          {/* button modalShop */}
+          <button
+            onClick={handleModalShow}
+            className="flex w-1/2 items-center justify-around  rounded-xl border border-cornflowerblue py-2 md:w-full "
+          >
+            <img src={shopcart} alt="" />
+            <span className="font-bold text-cornflowerblue">
+              $ {totalPrice}
+            </span>
+            <img
+              ref={img}
+              src={arrowDropdown}
+              alt=""
+              className="  transition-transform"
+            />
+          </button>
+          {/* contenedor modalShop */}
+          <div
+            ref={modalShop}
+            className=" z-10 mt-2 hidden  w-full rounded-b-lg bg-blue-100 group-hover:block  md:absolute md:right-0 md:top-10 md:h-80  md:w-[500px] lg:h-96 2xl:left-0"
+          >
+            <div className=" flex w-full items-center justify-between border-b border-black px-4 py-1 ">
+              <p className="inline-block">{products.length} productos</p>
+              <span onClick={handleDelete}>
+                <img
+                  src={deleteBtn}
+                  className="w-7 cursor-pointer hover:scale-105"
+                  alt="vaciar carrito"
+                />
+              </span>
+            </div>
+            <div
+              className={`flex h-40 flex-col gap-2 ${
+                products.length && "overflow-scroll overflow-x-hidden"
+              }  py-3 md:h-56 lg:h-[340px] `}
+            >
+              {products?.map((prod) => (
+                <Subcard prod={prod} key={prod.id} />
+              ))}
+            </div>
+            <hr />
+            <div className="flex items-center justify-between rounded-b-lg bg-blue-200 p-2 ">
+              <Link to="/shop/checkout">
+                <LinkButton component={"Confirmar Compra"} />
+              </Link>
+              <h2 className="border-blue inline-block rounded-md border py-2 px-4">
+                Total: ${totalPrice}
+              </h2>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
   );
 }
 
-export default NavBar;
-
-/* 
-  <nav className="flex flex-col lg:flex lg:justify-between ">
-      <div className="bg-cyan-700 flex items-center justify-between bg-primary px-2 py-2 ">
-        <div className="">
-          <img src={react} alt="" />
-        </div>
-
-        <div>
-          <NavLink to="/services" className="">
-            servicios
-          </NavLink>
-        </div>
-
-        <div className="">
-          <button
-            onClick={toggleBar}
-            type="button"
-            className="text-white focus:bg-cyan-0 ml-3 inline-flex items-center rounded-lg p-2  text-sm focus:outline-none focus:ring-2  lg:hidden"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              class="h-6 w-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"></path>
-            </svg>
-          </button>
-        </div>
-      </div>
-
-      <div
-        ref={toolBar}
-        className="bg-cyan-500 mr-2 hidden w-1/2 self-end rounded-b-xl py-1 px-2 lg:block"
-      >
-        <div className="flex flex-col items-center justify-center gap-1 ">
-          <div>
-            <SearchForm />
-          </div>
-          <div className="flex h-14 flex-col items-center justify-between">
-            <DropdownHelp />
-            <DropdownAccount />
-          </div>
-        </div>
-      </div>
-    </nav>
-
-*/
+export default NavBarPrueba;
--- client/src/components/NavBar/components/DropdownUser.jsx
+++ client/src/components/NavBar/components/DropdownUser.jsx
@@ -1,28 +1,187 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import user from "../../../assets/general/profile.svg";
 import { getUserApi } from "../../../redux/features/users/usersActions";
 import axios from "axios";
+import arrowDropdown from "../../../assets/arrows/arrowDropdown.svg";
 
 function DropdownUser() {
   const id = localStorage.getItem("id");
   const dispatch = useDispatch();
-
+  // traemos la data del usuario en redux
   const userDetailId = useSelector((state) => state.User?.userId);
-
+  // buscar usuario por id
   useEffect(() => {
     dispatch(getUserApi(id));
   }, []);
-  const prueba = () => {
+  const logOut = () => {
     axios.post("user/logout");
     localStorage.removeItem("token");
     localStorage.removeItem("id");
     window.location.reload();
   };
+  const userMenu = useRef(null);
+  const arrow = useRef(null);
+
+  const toggleUserMenu = () => {
+    userMenu.current.classList.toggle("hidden");
+    arrow.current.classList.toggle("rotate-180");
+  };
+  let activeClassName = "border-t border-white ";
 
   return (
-    <div className="group  relative flex items-center gap-2 md:order-2 lg:flex-col">
+    <div className="flex flex-col py-3 lg:w-full">
+      <button
+        onClick={toggleUserMenu}
+        className="group flex w-full items-center gap-1 lg:justify-center"
+        type="button"
+      >
+        <img src={user} alt="" className="" />
+        <span className="font-bold uppercase text-cornflowerblue">usuario</span>
+        <img
+          ref={arrow}
+          src={arrowDropdown}
+          alt=""
+          className="transition-transform"
+        />
+      </button>
+
+      <ul
+        ref={userMenu}
+        className="ml-7 hidden list-none overflow-hidden border border-cornflowerblue py-1 text-left text-cornflowerblue shadow-lg lg:absolute lg:top-12 lg:z-50 lg:ml-0 lg:w-40 lg:bg-russianviolet lg:p-2 xl:right-6 2xl:right-5 2xl:w-1/3  "
+      >
+        <li>
+          <div className="px-4 py-3">
+            <span className="block text-sm text-gray-900 dark:text-white">
+              {userDetailId?.name}
+            </span>
+            <span className="block truncate text-sm font-medium text-gray-500 dark:text-gray-400">
+              {userDetailId?.mail}
+            </span>
+          </div>
+        </li>
+        {/* Link to Profile */}
+        <li className="mb-1 py-1">
+          <NavLink
+            to={`profile/${id}`}
+            className="flex w-full gap-3 text-cornflowerblue lg:inline-block"
+          >
+            {({ isActive }) => (
+              <div
+                className={`${
+                  isActive ? activeClassName : undefined
+                } flex w-full gap-3 pb-1`}
+              >
+                <span className=" font-bold ">Perfil</span>
+              </div>
+            )}
+          </NavLink>
+        </li>
+        {/* Link to LogOut */}
+        <li className="py-1">
+          <NavLink
+            onClick={logOut}
+            className="flex w-full gap-3 text-cornflowerblue lg:inline-block"
+          >
+            {({ isActive }) => (
+              <div
+                className={`${
+                  isActive ? activeClassName : undefined
+                } flex w-full gap-3 pb-1`}
+              >
+                <span className=" font-bold ">Cerrar Sesión</span>
+              </div>
+            )}
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default DropdownUser;
+
+/* 
+
+<div className="flex flex-col py-3 lg:w-full">
+          <button
+            onClick={toggleUserMenu}
+            className="group flex w-full items-center gap-1 lg:justify-center"
+            type="button"
+          >
+            <img src={user} alt="" className="" />
+            <span className="font-bold uppercase text-cornflowerblue">
+              usuario
+            </span>
+            <img
+              ref={arrow}
+              src={arrowDropdown}
+              alt=""
+              className="transition-transform"
+            />
+          </button>
+          
+
+          <ul
+            ref={userMenu}
+            className="ml-7 hidden list-none overflow-hidden border border-cornflowerblue py-1 text-left text-cornflowerblue shadow-lg lg:absolute lg:top-12 lg:z-50 lg:ml-0 lg:w-40 lg:bg-russianviolet lg:p-2 xl:right-6 2xl:right-5"
+          >
+          <li><div className="px-4 py-3">
+          <span className="block text-sm text-gray-900 dark:text-white">
+            {userDetailId?.name}
+          </span>
+          <span className="block truncate text-sm font-medium text-gray-500 dark:text-gray-400">
+            {userDetailId?.mail}
+          </span>
+        </div></li>
+            {/* Link to Profile *
+            <li className="mb-1 py-1">
+              <NavLink
+                to={`profile/${id}`}
+                className="flex w-full gap-3 text-cornflowerblue lg:inline-block"
+              >
+                {({ isActive }) => (
+                  <div
+                    className={`${
+                      isActive ? activeClassName : undefined
+                    } flex w-full gap-3 pb-1`}
+                  >
+                    <span className=" font-bold ">Perfil</span>
+                  </div>
+                )}
+              </NavLink>
+            </li>
+            {/* Link to LogOut *
+            <li className="py-1">
+              <NavLink
+                to="/shop"
+                className="flex w-full gap-3 text-cornflowerblue lg:inline-block"
+              >
+                {({ isActive }) => (
+                  <div
+                    className={`${
+                      isActive ? activeClassName : undefined
+                    } flex w-full gap-3 pb-1`}
+                  >
+                    <span className=" font-bold ">Cerrar Sesión</span>
+                  </div>
+                )}
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+-------------------------------------------------------
+ <button
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
+              onClick={logOut}
+            >
+              Cerrar Sesion
+            </button>
+----------------------------------------------------------------
+
+<div className="group  relative flex items-center gap-2 md:order-2 lg:flex-col">
       <button
         type="button"
         className="mr-3 flex rounded-full  text-sm focus:ring-4 focus:ring-gray-300  md:mr-0"
@@ -62,7 +221,7 @@ function DropdownUser() {
           <li>
             <button
               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-              onClick={prueba}
+              onClick={logOut}
             >
               Cerrar Sesion
             </button>
@@ -70,7 +229,5 @@ function DropdownUser() {
         </ul>
       </div>
     </div>
-  );
-}
 
-export default DropdownUser;
+*/
