03b7beefc2b28e63a49c0573bfedcdef0fb53758
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-02-23 20:42:43 -0300

    agregado carrousel y preview de cloudinary en creacion de productos

--- client/src/pages/ecommerce/Forms/FormCreateProduct/FormCreateProduct.jsx
+++ client/src/pages/ecommerce/Forms/FormCreateProduct/FormCreateProduct.jsx
@@ -6,6 +6,8 @@ import Swal from "sweetalert2";
 import { Carousel } from "flowbite-react";
 
 function FormCreateProduct() {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+  const [imgFile, setImgFile] = useState(null);
   const [formComplete, setFormComplete] = useState(false);
   const [img, setImg] = useState(null);
   const [form, setForm] = useState({
@@ -36,8 +38,15 @@ function FormCreateProduct() {
   });
   const changeHandlerImg = (e) => {
     setImg(e.target.files[0]);
-    console.log(e.target.files[0]);
+    const files = e.target.files;
+    const fileArray = [];
+    for (let i = 0; i < files.length; i++) {
+      fileArray.push(URL.createObjectURL(files[i]));
+    }
+    setSelectedFiles(fileArray);
+    setImgFile(fileArray);
   };
+
   const changeHandler = (e) => {
     let property = e.target.name;
     let value = e.target.value;
@@ -87,14 +96,17 @@ function FormCreateProduct() {
         text: "Por favor, revisa los campos del formulario",
       });
     }
+    console.log(img.name);
+    console.log(img);
   };
+
   return (
     // este es el contenedor completo
     <div className="flex min-h-screen w-full justify-center bg-[url('https://petfood.com.ar/img/cms/symphony.png')] pt-28 pb-10 ">
       {/* este es el form completo */}
       <form
         onSubmit={submitHandler}
-        className="mt-10 flex max-h-screen w-full flex-col items-center   rounded-xl  bg-gradient-to-r from-cornflowerblue to-russianviolet p-3 drop-shadow-2xl md:w-3/5 lg:h-auto "
+        className="mt-10 flex max-h-screen w-full flex-col items-center rounded-xl bg-russianviolet p-3 text-lg font-extrabold text-cornflowerblue drop-shadow-2xl md:w-3/5 lg:h-auto "
       >
         <h3 className="mb-6">Sube tus productos con los datos solicitados</h3>
         {/* aca empieza el div con fondo azul que contiene la estructura del form */}
@@ -256,26 +268,14 @@ function FormCreateProduct() {
           {/* aca empieza el div con las imagenes del producto y la descripcion de la mitad derecha */}
           <div className="flex w-1/2 flex-col justify-center ">
             {/* aca empieza el div con el carrousel */}
-            <div className=" h-1/2 ">
-              {" "}
-              <Carousel>
-                <img
-                  src={img}
-                  alt="aca se mostrara la imagen subida"
-                  className="min-w-1/4  aspect-square  bg-contain"
-                />
-                <img
-                  alt="aca se mostrara la imagen subida"
-                  className="min-w-1/4 aspect-square  "
-                />
-                <img
-                  alt="aca se mostrara la imagen subida"
-                  className="min-w-1/4 aspect-square  "
-                />
-                <img
-                  alt="aca se mostrara la imagen subida"
-                  className="min-w-1/4 aspect-square  "
-                />
+            <div className="flex h-1/2 justify-center">
+              <Carousel className="w-10/12 ">
+                {selectedFiles &&
+                  selectedFiles.map((file) => (
+                    <picture className="flex aspect-square h-full items-center justify-center ">
+                      <img src={file} alt="" key={img.id} className="h-full" />
+                    </picture>
+                  ))}
               </Carousel>
             </div>
             {/* aca empieza el div con la descripcion */}
@@ -283,10 +283,10 @@ function FormCreateProduct() {
               <div className="group relative z-0 mb-6 flex h-11 w-full">
                 <input
                   type="file"
+                  multiple
                   accept="image/*"
                   onChange={changeHandlerImg}
                 />
-
                 <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-gray-900 dark:text-gray-400 peer-focus:dark:text-gray-900">
                   Imagen:
                 </label>
