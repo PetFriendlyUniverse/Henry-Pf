c799bfbc4af2cbd53b1644750910445e553d71a3
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-02-22 21:19:54 -0300

    .

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const { conn } = require("./src/db.js");
 const port = process.env.port || 3001;
 
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(port, () => {
     console.log("%s listening at 3001");
   });
 });
