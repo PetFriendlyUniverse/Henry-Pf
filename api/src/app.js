65f66cc37f31be5255733a69083d09e3eb78be45
Stefano Gemelli <96079828+stefanogemelli@users.noreply.github.com>
2023-02-26 15:50:07 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -22,7 +22,10 @@ cloudinary.config({
 
 server.use(express.json());
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    ORIGIN ? `http://${ORIGIN}:5173` : REMOTE_ORIGIN
+  ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
