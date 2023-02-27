f019a90fe69c4de0abf262eabe52c1cdd4e7d24c
Pablo Amico <114095359+pablosecuen@users.noreply.github.com>
2023-02-27 11:17:26 -0300

    Update FormModifyStore.jsx
    agregue el import de useEffect que faltaba

--- client/src/pages/ecommerce/Forms/FormModifyStore/FormModifyStore.jsx
+++ client/src/pages/ecommerce/Forms/FormModifyStore/FormModifyStore.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import Swal from "sweetalert2";
