c7af877391e597c430dbf000392c638b00129fd1
Stefano Gemelli <96079828+stefanogemelli@users.noreply.github.com>
2023-02-27 00:36:39 -0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,8 +9,8 @@ import { Provider } from "react-redux";
 
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001";
-// axios.defaults.baseURL = "https://petfriendly-backend.onrender.com";
+// axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://petfriendly-backend.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
