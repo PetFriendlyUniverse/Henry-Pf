5bc45cdd45fcf5428af9f5326fdf08b45e7f7f82
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-02-27 11:37:52 -0300

    solo borre un import que no se usaba

--- client/src/pages/ecommerce/Forms/FormModifyStore/FormModifyStore.jsx
+++ client/src/pages/ecommerce/Forms/FormModifyStore/FormModifyStore.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import axios from "axios";
 import Swal from "sweetalert2";
 import { ValidateStore } from "../Validations/ValidateStore";
