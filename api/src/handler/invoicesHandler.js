84f3b68fe29885c5ee9ab87cf32088234df73010
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-02-28 15:25:22 -0300

    Update invoicesHandler.js

--- api/src/handler/invoicesHandler.js
+++ api/src/handler/invoicesHandler.js
@@ -24,9 +24,14 @@ const getInvoiceIdHandler = async (req, res) => {
 };
 
 const postInvoiceHandler = async (req, res) => {
-  const { UserId, products } = req.body; //products = {product_id: amount,product_id: amount, ...}
+  const { UserId, products, paymentId, merchantOrder } = req.body; //products = [{id,quantity,unitPrice }]
   try {
-    let invoice = await createInvoice(UserId, products);
+    let invoice = await createInvoice(
+      UserId,
+      products,
+      paymentId,
+      merchantOrder
+    );
     return res.status(200).json(invoice);
   } catch (error) {
     return res.status(404).json({ error: error.message });
