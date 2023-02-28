a3deeb6cdbfeea5bc6408d53e54f2289265cdcd1
AgustinChavero <agustin199orellano@gmail.com>
2023-02-28 18:04:10 -0300

    creacion del archivo tickets y cards de cuentas

---
+++ client/src/components/DashboardAdmin/components/Tickets.jsx
@@ -0,0 +1,11 @@
+import Borrador from "../../../assets/borrador/construction.png";
+
+function Tickets() {
+  return (
+    <div className="flex justify-center pb-10">
+      <img src={Borrador} alt="borrador" className="w-96" />
+    </div>
+  );
+}
+
+export default Tickets;
