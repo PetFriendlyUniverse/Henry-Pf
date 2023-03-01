38469e3aa78330ac94c8a98a0f457d3e0ce6be0b
AgustinChavero <agustin199orellano@gmail.com>
2023-03-01 12:33:03 -0300

    Agustin - Estadisticas funcionales

--- client/src/components/DashboardAdmin/components/Graphs.jsx
+++ client/src/components/DashboardAdmin/components/Graphs.jsx
@@ -4,34 +4,34 @@ import "@tremor/react/dist/esm/tremor.css";
 function Graphs() {
   const chartdata = [
     {
-      date: "Jan 22",
-      SemiAnalysis: 2890,
-      "The Pragmatic Engineer": 2338,
+      date: "Enero 22",
+      Tiendas: 2890,
+      Guarderias: 2338,
     },
     {
-      date: "Feb 22",
-      SemiAnalysis: 2756,
-      "The Pragmatic Engineer": 2103,
+      date: "Febrero 22",
+      Tiendas: 2756,
+      Guarderias: 2103,
     },
     {
-      date: "Mar 22",
-      SemiAnalysis: 3322,
-      "The Pragmatic Engineer": 2194,
+      date: "Marzo 22",
+      Tiendas: 3322,
+      Guarderias: 2194,
     },
     {
-      date: "Apr 22",
-      SemiAnalysis: 3470,
-      "The Pragmatic Engineer": 2108,
+      date: "Abril 22",
+      Tiendas: 3470,
+      Guarderias: 2108,
     },
     {
-      date: "May 22",
-      SemiAnalysis: 3475,
-      "The Pragmatic Engineer": 1812,
+      date: "Mayo 22",
+      Tiendas: 3475,
+      Guarderias: 1812,
     },
     {
-      date: "Jun 22",
-      SemiAnalysis: 3129,
-      "The Pragmatic Engineer": 1726,
+      date: "Junio 22",
+      Tiendas: 3129,
+      Guarderias: 1726,
     },
   ];
   const dataFormatter = function (number) {
@@ -41,10 +41,10 @@ function Graphs() {
   return (
     <div>
       <Card>
-        <Title>Newsletter revenue over time (USD)</Title>
+        <Title>Grafico de ventas del ultimo año</Title>
         <AreaChart
           data={chartdata}
-          categories={["SemiAnalysis", "The Pragmatic Engineer"]}
+          categories={["Tiendas", "Guarderias"]}
           dataKey="date"
           height="h-72"
           colors={["indigo", "cyan"]}
