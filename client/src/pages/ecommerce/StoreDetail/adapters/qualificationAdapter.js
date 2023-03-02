4af67bb641153b964bace748173031c2ce818067
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-03-02 11:31:35 -0300

    StoreDetail - render dinámico de info del backend

--- client/src/pages/ecommerce/StoreDetail/adapters/qualificationAdapter.js
+++ client/src/pages/ecommerce/StoreDetail/adapters/qualificationAdapter.js
@@ -1,17 +1,38 @@
 const adapter = {
   support: {
-    3: ["Brinda buena atención", "Sus clientes están satisfechos"],
-    2: ["Brinda la atención justa", "Sus clientes no están muy satisfechos"],
-    1: ["No brinda buena atención", "Sus clientes estan descontentos"],
+    5: ["Brinda una excelente atención", "Sus clientes están muy satisfechos"],
+    4: ["Brinda muy buena atención", "Sus clientes están satisfechos"],
+    3: ["Brinda una atención regular", "Sus clientes están satisfechos"],
+    2: ["Brinda una mala atención", "Sus clientes no están satisfechos"],
+    1: ["No brinda atención", "Sus clientes estan descontentos"],
   },
   dispatchtime: {
-    3: ["", ""],
-    2: ["", ""],
-    1: ["", ""],
+    5: [
+      "Despacha sus productos en excelente tiempo y forma",
+      "Sus clientes están muy satisfechos",
+    ],
+    4: [
+      "Despacha sus productos en muy buen tiempo y forma",
+      "Sus clientes están satisfechos",
+    ],
+    3: [
+      "Despacha sus productos en tiempo y forma",
+      "Sus clientes están satisfechos",
+    ],
+    2: [
+      "Despacha sus productos con algo de retraso y en condiciones regulares",
+      "Sus clientes están algo insatisfechos",
+    ],
+    1: [
+      "No despacha sus productos en el tiempo acordado y en malas condiciones",
+      "Sus clientes están muy insatisfechos",
+    ],
   },
 };
 
-export const qualificationAdapter = (type, qualification, titleOrMessage) =>
-  titleOrMessage == "title"
-    ? adapter[type][qualification][0]
-    : adapter[type][qualification][1];
+export const qualificationAdapter = (type, qualification, titleOrMessage) => {
+  const qual = Math.ceil(qualification);
+  return titleOrMessage == "title"
+    ? adapter[type][qual][0]
+    : adapter[type][qual][1];
+};
