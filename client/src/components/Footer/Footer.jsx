f7a66a8c58c7b2a6778f77f7a8676fabb200a895
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-03-02 12:20:12 -0300

    login responsive

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className=" bg-gray-100 text-center text-gray-600 lg:text-left">
+    <footer className=" overflow-hidden bg-gray-100 text-center text-gray-600 lg:text-left">
       <div className="flex items-center justify-center border-b border-gray-300 p-6 lg:justify-between">
         <div className="mr-12 hidden lg:block">
           <span>Conéctate con nosotros en las redes sociales:</span>
