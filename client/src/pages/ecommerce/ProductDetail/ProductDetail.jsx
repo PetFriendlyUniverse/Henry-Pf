51ceef466b72e4e69404dcc4b713bb6a11f6758d
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-03-04 19:08:53 -0300

    limpieza masiva de divs

--- client/src/pages/ecommerce/ProductDetail/ProductDetail.jsx
+++ client/src/pages/ecommerce/ProductDetail/ProductDetail.jsx
@@ -83,95 +83,69 @@ function ProductDetail() {
             <img src={product?.img} alt="" className="w-[460px] " />
           </picture>
           <div className="inline-block w-full lg:w-6/12 ">
-            <div>
-              <h1 className="font-bold md:text-2xl lg:text-3xl">
-                {product?.name}
-              </h1>
-              <button className="text-xs">Old Pince</button>
-              <span className="text-xs">
-                -Código del producto:{product?.id}
-              </span>
-              {/* --------------------------------------     [ Stars ]     ------------------------------------------ */}
-              <div className="-mt-3">
-                <QualificationStars rating={rating} />
-              </div>
+            <h2 className="font-bold md:text-2xl lg:text-3xl">
+              {product?.name}
+            </h2>
+            <button className="text-xs">Old Pince</button>
+            <span className="text-xs">-Código del producto:{product?.id}</span>
+            {/* --------------------------------------     [ Stars ]     ------------------------------------------ */}
+            <div className="-mt-3">
+              <QualificationStars rating={rating} />
             </div>
-
             <div className="flex flex-col flex-wrap items-start gap-4 md:flex-row md:gap-8">
-              <div>
-                <h3>{"$" + product?.price}</h3>
-              </div>
-              <div>
-                <button className="flex items-center text-[12px] font-bold uppercase text-pink-700">
-                  <img src={cardCredit} alt="" /> Ver medios de pagos
-                </button>
-                <button className="flex  items-center text-[12px] font-bold uppercase text-pink-700">
-                  <img src={transport} alt="" />
-                  Producto con envio gratis
-                </button>
-              </div>
-            </div>
-            <div>
-              <span className="text-xs">({product?.price + "x Kg"})</span>
-              <h3>Seleccione Tamaño:</h3>
-              <div className=" md:w-1/5 lg:w-1/4">
-                <MoarButton component={product?.weight + "kg"} />
-              </div>
-            </div>
-            <div>
-              <span> Seleccione Cantidad:</span>
-              <div className="w-1/2">
-                <CountProduct
-                  handleClickDeduct={handleClickDeduct}
-                  handleClickAdd={handleClickAdd}
-                  value={amount}
-                />
-              </div>
-              <button
-                className="my-2 rounded-lg bg-[#4dbb47] py-2 px-4 md:min-w-full lg:w-1/2"
-                onClick={() => handleClickAddProduct(product)}
-              >
-                Añadir al carrito
+              <h3>{"$" + product?.price}</h3>
+              <button className="flex items-center text-[12px] font-bold uppercase text-pink-700">
+                <img src={cardCredit} alt="" /> Ver medios de pagos
+              </button>
+              <button className="flex  items-center text-[12px] font-bold uppercase text-pink-700">
+                <img src={transport} alt="" />
+                Producto con envio gratis
               </button>
             </div>
+            <span className="text-xs">({product?.price + "x Kg"})</span>
+            <h3>Seleccione Tamaño:</h3>
+            <div className=" md:w-1/5 lg:w-1/4">
+              <MoarButton component={product?.weight + "kg"} />
+            </div>
+            <span> Seleccione Cantidad:</span>
+            <div className="w-1/2">
+              <CountProduct
+                handleClickDeduct={handleClickDeduct}
+                handleClickAdd={handleClickAdd}
+                value={amount}
+              />
+            </div>
+            <button
+              className="my-2 rounded-lg bg-[#4dbb47] py-2 px-4 md:min-w-full lg:w-1/2"
+              onClick={() => handleClickAddProduct(product)}
+            >
+              Añadir al carrito
+            </button>
             <div className="text-sm">
-              <div>
-                Opciones de Envío
-                <span></span>
-              </div>
-              <div> Envío a Domicilio </div>
-              <div className="md:text-xs lg:text-sm ">
+              <p>Opciones de Envío</p>
+              <p> Envío a Domicilio </p>
+              <p className="md:text-xs lg:text-sm ">
                 Envío gratis +$7500 en CABA y zonas de GBA. No aplica para
-                Envíos al Interior
-              </div>
-
-              <div>
-                Envío Flash
-                <span></span>
-              </div>
-              <div className="text-xs">
+                envíos al Interior
+              </p>
+              <p>Envío Flash</p>
+              <p className="text-xs">
                 Envíos a todo CABA y zonas seleccionadas de AMBA. Envíos a todo
                 CABA y zonas seleccionadas de AMBA. Envíos a todo CABA y zonas
                 seleccionadas de AMBA.
-              </div>
+              </p>
             </div>
-            <div>
-              <div>
-                <div>Retiro en Sucursal</div>
-                <div className="text-xs">
-                  Retirá sin cargo por tu sucursal preferida
-                </div>
-              </div>
-            </div>
+            <p>Retiro en Sucursal</p>
+            <p className="text-xs">
+              Retirá sin cargo por tu sucursal preferida
+            </p>
           </div>
         </div>
       </div>
       <div className="mt-20 h-full min-h-[200px] w-full bg-gray-100">
         <Tabs.Group aria-label="Tabs with underline" style="underline">
           <Tabs.Item title="Descripción">
-            <div>
-              <p>{product?.description}</p>
-            </div>
+            <p>{product?.description}</p>
           </Tabs.Item>
           <Tabs.Item title="Comentarios">
             <div>Reseñas de clientes</div>
