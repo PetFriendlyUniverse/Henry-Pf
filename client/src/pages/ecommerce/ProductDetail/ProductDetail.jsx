fe79da5985eea1159b99c20fcc998e0d1de6bd1a
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-03-04 18:58:18 -0300

    reubicacion e implementacion de QualificationStars

--- client/src/pages/ecommerce/ProductDetail/ProductDetail.jsx
+++ client/src/pages/ecommerce/ProductDetail/ProductDetail.jsx
@@ -1,24 +1,17 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import Swal from "sweetalert2";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Tabs } from "flowbite-react";
 import MoarButton from "../../../components/Button/MoarButton";
 import CountProduct from "../../../components/CountProduct/CountProduct";
 import cardCredit from "../../../assets/cardCredit/cardCredit.svg";
 import transport from "../../../assets/transport/transport.svg";
-import {
-  getProductsId,
-  deleteProductId,
-} from "../../../redux/features/products/productsActions";
-import {
-  clearProductId,
-  setShopCart,
-} from "../../../redux/features/products/productsSlice";
-import "react-loading-skeleton/dist/skeleton.css";
+import { setShopCart } from "../../../redux/features/products/productsSlice";
 import StoreDetail from "./components/StoreDetail/StoreDetail";
 import ProductDetailSkeleton from "./ProductDetailSkeleton";
 import useGetProductInfo from "./hooks/useGetProductInfo";
+import QualificationStars from "./components/QualificationStars/QualificationStars";
 
 function ProductDetail() {
   window.scrollTo(0, 0);
@@ -30,18 +23,9 @@ function ProductDetail() {
   const shopCartProducts = useSelector((state) => state.Products?.shopCart);
   const auxCartAmount = shopCartProducts[id] ? shopCartProducts[id].amount : 0;
 
-  // const { productId } = useSelector((state) => state.Products);
-  // const { product, comments, qualification } = productId;
   const [product, comments, qualification, loading] = useGetProductInfo(id);
-
-  // useEffect(() => {
-  //   dispatch(getProductsId(id));
-  //   return () => {
-  //     dispatch(clearProductId());
-  //   };
-  // }, []);
-
   const auxCartStock = product?.stock;
+  const rating = parseFloat(qualification?.[0].avg).toFixed(2);
 
   const handleClickDeduct = () => {
     if (amount > 1) setAmount(amount - 1);
@@ -108,57 +92,8 @@ function ProductDetail() {
                 -Código del producto:{product?.id}
               </span>
               {/* --------------------------------------     [ Stars ]     ------------------------------------------ */}
-              <div className="flex items-center">
-                <svg
-                  aria-hidden="true"
-                  className="h-5 w-5 text-yellow-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <title>First star</title>
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                </svg>
-                <svg
-                  aria-hidden="true"
-                  className="h-5 w-5 text-yellow-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <title>Second star</title>
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                </svg>
-                <svg
-                  aria-hidden="true"
-                  className="h-5 w-5 text-yellow-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <title>Third star</title>
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                </svg>
-                <svg
-                  aria-hidden="true"
-                  className="h-5 w-5 text-yellow-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <title>Fourth star</title>
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                </svg>
-                <svg
-                  aria-hidden="true"
-                  className="h-5 w-5 text-gray-300 dark:text-gray-500"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <title>Fifth star</title>
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                </svg>
+              <div className="-mt-3">
+                <QualificationStars rating={rating} />
               </div>
             </div>
 
--- client/src/pages/ecommerce/ProductDetail/ProductDetailSkeleton.jsx
+++ client/src/pages/ecommerce/ProductDetail/ProductDetailSkeleton.jsx
@@ -1,4 +1,5 @@
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 function ProductDetailSkeleton() {
   return (
