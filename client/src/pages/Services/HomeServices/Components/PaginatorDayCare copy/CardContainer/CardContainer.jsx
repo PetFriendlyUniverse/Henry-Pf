6fe5dfdb06c00c09f9932c019bb5e34ce31c6a69
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-03-04 18:18:10 -0300

    card container de day care con mapeado de props de sus cards

--- client/src/pages/Services/HomeServices/Components/PaginatorDayCare copy/CardContainer/CardContainer.jsx
+++ client/src/pages/Services/HomeServices/Components/PaginatorDayCare copy/CardContainer/CardContainer.jsx
@@ -1,20 +1,31 @@
+import Loader from "../../../../../../components/Loader/Loader";
+import useGetWalkers from "../../../../../../hooks/useGetWalkers";
 import Card from "./Card/Card";
-// const [loading, services] = useGetProducts();
 
 function CardContainer() {
-  return (
-    <div className="mx-auto flex h-full w-full items-center justify-center justify-items-center gap-7 border-2 border-black sm:w-4/5 sm:grid-cols-2 md:max-w-2xl lg:m-0 lg:min-h-fit  lg:w-full  lg:grid-cols-3 lg:gap-x-5 lg:gap-y-6 xl:max-w-5xl xl:grid-cols-4 xl:gap-x-2  2xl:max-w-7xl  2xl:gap-x-10 2xl:gap-y-12  ">
-      {/* {products?.length === 0 ? (
-      <div className="min-h-[50vh]">
-        <h2>No se encotraron coincidencias con los datos solicitados</h2>
+  const [loading, walkers] = useGetWalkers();
+  console.log(walkers);
+
+  if (loading)
+    return (
+      <div className="flex h-screen w-full flex-col content-center items-center py-72">
+        <Loader />
       </div>
-    ) : (
-      products?.map((product) => {
-        return ( */}
-      <Card />
-      {/* );
-      })
-    )} */}
+    );
+
+  return (
+    <div className="mx-auto flex h-full w-full flex-wrap items-center justify-center justify-items-center gap-7 border-2 border-black sm:w-4/5 sm:grid-cols-2 md:max-w-2xl lg:m-0 lg:min-h-fit  lg:w-full  lg:grid-cols-3 lg:gap-x-5 lg:gap-y-6 xl:max-w-5xl xl:grid-cols-4 xl:gap-x-2  2xl:max-w-7xl  2xl:gap-x-10 2xl:gap-y-12  ">
+      {walkers?.length === 0 ? (
+        <div className="min-h-[50vh]">
+          <h2>No se encotraron coincidencias con los datos solicitados</h2>
+        </div>
+      ) : (
+        walkers?.map((w) => {
+          return (
+            <Card key={w?.id} id={w?.id} name={w?.name} price={w?.price} />
+          );
+        })
+      )}
     </div>
   );
 }
