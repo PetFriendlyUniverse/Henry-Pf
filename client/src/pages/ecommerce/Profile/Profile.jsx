ed4f196462029f676ba65128be33f097bccceec5
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-03-04 15:08:14 -0300

    reestructura final de profile

--- client/src/pages/ecommerce/Profile/Profile.jsx
+++ client/src/pages/ecommerce/Profile/Profile.jsx
@@ -87,20 +87,21 @@ function Profile() {
   }, []);
   return (
     <div className="flex min-h-[80vh] justify-start  bg-adopcion pb-28 pt-10 lg:items-center lg:justify-center">
-      <div className="flex h-full w-full flex-col items-center justify-end gap-3 rounded-2xl  px-6 lg:w-full lg:max-w-5xl lg:flex-row lg:justify-start   lg:gap-16">
+      <div className="flex h-full w-full flex-col items-center justify-end gap-3 rounded-2xl  px-6 lg:w-full  lg:flex-row lg:justify-start   lg:gap-10 2xl:px-16 ">
+        {/* container profile */}
         <div className="h-full w-full max-w-xs rounded-2xl border-4 border-cornflowerblue  p-3 xl:w-96 ">
           <PhotoName img={user?.img} name={user?.name} />
           <div className="mb-4 mt-4   dark:border-gray-700">
             <ul className="-mb-px text-center text-sm font-medium">
               <li
-                className={`mr-2 rounded-lg border-b-2 bg-slate-50 hover:cursor-pointer hover:bg-violet-900 hover:text-slate-300 ${
+                className={`mr-2 rounded-lg border-b-2 bg-slate-50 hover:cursor-pointer hover:bg-ultravioletLight hover:text-black active:shadow-inner active:shadow-black ${
                   showInfo == "profile" &&
-                  "hover:bg-  bg-ultraviolet text-slate-300 hover:text-slate-300"
+                  "  bg-ultraviolet text-slate-300 hover:text-slate-300"
                 } hover:text-gray-500`}
               >
                 <button
-                  id="Perfil"
                   onClick={handleShowInfo}
+                  id="Perfil"
                   name="profile"
                   className={`inline-block  p-4 tracking-wider ${
                     showInfo == "profile" &&
@@ -111,31 +112,55 @@ function Profile() {
                   Perfil
                 </button>
               </li>
-              <li className="mr-2 rounded-lg border-b-2 bg-slate-50  hover:bg-slate-100 hover:text-gray-500">
+              <li
+                className={`mr-2 rounded-lg border-b-2 bg-slate-50 hover:cursor-pointer hover:bg-ultravioletLight hover:text-black active:shadow-inner active:shadow-black ${
+                  showInfo == "contact" &&
+                  "  bg-ultraviolet text-slate-300 hover:text-slate-300"
+                } hover:text-gray-500`}
+              >
                 <button
                   onClick={handleShowInfo}
                   name="contact"
-                  className="inline-block border-b-2 border-transparent p-4 "
+                  className={`inline-block  p-4 tracking-wider ${
+                    showInfo == "contact" &&
+                    "tracking-wider underline underline-offset-4"
+                  }`}
                   title="aqui puedes ver tu informacion de contacto"
                 >
                   Contacto
                 </button>
               </li>
-              <li className="mr-2 rounded-lg border-b-2 bg-slate-50  hover:bg-slate-100 hover:text-gray-500">
+              <li
+                className={`mr-2 rounded-lg border-b-2 bg-slate-50 hover:cursor-pointer hover:bg-ultravioletLight hover:text-black active:shadow-inner active:shadow-black ${
+                  showInfo == "location" &&
+                  "  bg-ultraviolet text-slate-300 hover:text-slate-300"
+                } hover:text-gray-500`}
+              >
                 <button
                   onClick={handleShowInfo}
                   name="location"
-                  className="inline-block p-4"
+                  className={`inline-block  p-4 tracking-wider ${
+                    showInfo == "location" &&
+                    "tracking-wider underline underline-offset-4"
+                  }`}
                   title="aqui puedes ver tus domicilios"
                 >
                   Direcciones
                 </button>
               </li>
-              <li className="mr-2 rounded-lg border-b-2 bg-slate-50  hover:bg-slate-100 hover:text-gray-500">
+              <li
+                className={`mr-2 rounded-lg border-b-2 bg-slate-50 hover:cursor-pointer hover:bg-ultravioletLight hover:text-black active:shadow-inner active:shadow-black ${
+                  showInfo == "purchase" &&
+                  "  bg-ultraviolet text-slate-300 hover:text-slate-300"
+                } hover:text-gray-500`}
+              >
                 <button
                   onClick={handleShowInfo}
                   name="purchase"
-                  className="inline-block p-4"
+                  className={`inline-block  p-4 tracking-wider ${
+                    showInfo == "purchase" &&
+                    "tracking-wider underline underline-offset-4"
+                  }`}
                   title="aqui puedes ver tu historial de compras"
                 >
                   Compras
@@ -172,7 +197,9 @@ function Profile() {
                 </li>
               )}
 
-              <li className="mr-2 rounded-lg border-b-2 bg-slate-50  hover:bg-slate-100 hover:text-gray-500">
+              <li
+                className={`mr-2 rounded-lg border-b-2 bg-ultraviolet text-slate-300 hover:cursor-pointer hover:bg-ultravioletLight hover:text-black  active:shadow-inner active:shadow-black`}
+              >
                 {/* Reveer esto de volver al hacer Health/Services */}
                 <Link to={`/shop`}>
                   <button className="inline-block p-4">Volver</button>
@@ -181,8 +208,8 @@ function Profile() {
             </ul>
           </div>
         </div>
-
-        <div className="flex h-full w-full flex-col  flex-wrap rounded-2xl border-4 border-cornflowerblue py-3   ">
+        {/* container main */}
+        <div className="flex h-full min-h-[450px] w-full flex-col  flex-wrap rounded-2xl border-4 border-cornflowerblue py-3   ">
           <div className="border-cornflowerblue p-3 lg:border-b-2">
             <h1 className="flex justify-center pl-2 text-3xl font-semibold">
               {titles[showInfo]}
@@ -232,6 +259,126 @@ function Profile() {
                 </button>
               </div>
             </div>
+          </div>
+        </div>
+        {/* container pets form */}
+        <div className="h-full w-full max-w-xs rounded-2xl border-4 border-cornflowerblue  p-3 xl:w-96 ">
+          <PhotoName img={user?.img} name={user?.name} />
+          <div className="mb-4 mt-4   dark:border-gray-700">
+            <ul className="-mb-px text-center text-sm font-medium">
+              <li
+                className={`mr-2 rounded-lg border-b-2 bg-slate-50 hover:cursor-pointer hover:bg-ultravioletLight hover:text-black active:shadow-inner active:shadow-black ${
+                  showInfo == "profile" &&
+                  "  bg-ultraviolet text-slate-300 hover:text-slate-300"
+                } hover:text-gray-500`}
+              >
+                <button
+                  onClick={handleShowInfo}
+                  id="Perfil"
+                  name="profile"
+                  className={`inline-block  p-4 tracking-wider ${
+                    showInfo == "profile" &&
+                    "tracking-wider underline underline-offset-4"
+                  }`}
+                  title="aqui puedes ver quien es el propietario de la cuenta"
+                >
+                  Perfil
+                </button>
+              </li>
+              <li
+                className={`mr-2 rounded-lg border-b-2 bg-slate-50 hover:cursor-pointer hover:bg-ultravioletLight hover:text-black active:shadow-inner active:shadow-black ${
+                  showInfo == "contact" &&
+                  "  bg-ultraviolet text-slate-300 hover:text-slate-300"
+                } hover:text-gray-500`}
+              >
+                <button
+                  onClick={handleShowInfo}
+                  name="contact"
+                  className={`inline-block  p-4 tracking-wider ${
+                    showInfo == "contact" &&
+                    "tracking-wider underline underline-offset-4"
+                  }`}
+                  title="aqui puedes ver tu informacion de contacto"
+                >
+                  Contacto
+                </button>
+              </li>
+              <li
+                className={`mr-2 rounded-lg border-b-2 bg-slate-50 hover:cursor-pointer hover:bg-ultravioletLight hover:text-black active:shadow-inner active:shadow-black ${
+                  showInfo == "location" &&
+                  "  bg-ultraviolet text-slate-300 hover:text-slate-300"
+                } hover:text-gray-500`}
+              >
+                <button
+                  onClick={handleShowInfo}
+                  name="location"
+                  className={`inline-block  p-4 tracking-wider ${
+                    showInfo == "location" &&
+                    "tracking-wider underline underline-offset-4"
+                  }`}
+                  title="aqui puedes ver tus domicilios"
+                >
+                  Direcciones
+                </button>
+              </li>
+              <li
+                className={`mr-2 rounded-lg border-b-2 bg-slate-50 hover:cursor-pointer hover:bg-ultravioletLight hover:text-black active:shadow-inner active:shadow-black ${
+                  showInfo == "purchase" &&
+                  "  bg-ultraviolet text-slate-300 hover:text-slate-300"
+                } hover:text-gray-500`}
+              >
+                <button
+                  onClick={handleShowInfo}
+                  name="purchase"
+                  className={`inline-block  p-4 tracking-wider ${
+                    showInfo == "purchase" &&
+                    "tracking-wider underline underline-offset-4"
+                  }`}
+                  title="aqui puedes ver tu historial de compras"
+                >
+                  Compras
+                </button>
+              </li>
+              {user.admin && (
+                <li className="mr-2 rounded-lg border-b-2 bg-slate-50 py-3  hover:bg-slate-100 hover:text-gray-500">
+                  <Link to={`/dashboardadmin`}>
+                    <button title="panel de admin">Panel de admin</button>
+                  </Link>
+                </li>
+              )}
+              {user.store && (
+                <li className="mr-2 rounded-lg border-b-2 bg-slate-50 py-3  hover:bg-slate-100 hover:text-gray-500">
+                  <Link to={`/profile/store/${user.storeId}`}>
+                    <button title="ingresa a tu tienda">Tienda</button>
+                  </Link>
+                </li>
+              )}
+              {user.walker && (
+                <li className="mr-2 rounded-lg border-b-2 bg-slate-50 py-3  hover:bg-slate-100 hover:text-gray-500">
+                  <Link to={`/profile/walker/${user.walkerId}`}>
+                    <button title="ingresa a tu perfil de paseador">
+                      Paseador
+                    </button>
+                  </Link>
+                </li>
+              )}
+              {user.daycare && (
+                <li className="mr-2 rounded-lg border-b-2 bg-slate-50 py-3  hover:bg-slate-100 hover:text-gray-500">
+                  <Link to={`/profile/daycare/${user.daycareId}`}>
+                    <button title="ingresa a tu guarderia">Guarderia</button>
+                  </Link>
+                </li>
+              )}
+
+              <li
+                className={`mr-2 rounded-lg border-b-2 bg-ultraviolet text-slate-300 hover:cursor-pointer hover:bg-ultravioletLight hover:text-black  active:shadow-inner active:shadow-black`}
+              >
+                {/* Reveer esto de volver al hacer Health/Services */}
+                <Link to={`/shop`}>
+                  <button className="inline-block p-4">Volver</button>
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
