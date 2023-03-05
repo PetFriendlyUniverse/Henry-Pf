462f1d4ca140025deede5aa098676fbb8f93d1b7
Benja <benyamin0714@gmail.com>
2023-03-05 18:56:16 -0300

    Benja - Dropdown sin productos y estilos

--- client/src/components/Paginator/components/Sort/Sort.jsx
+++ client/src/components/Paginator/components/Sort/Sort.jsx
@@ -20,19 +20,19 @@ function Sort() {
 
   return (
     <>
-      <div className="flex w-4/5">
-        <div className="w-1/2">
+      <div className="flex w-4/5 items-center gap-2">
+        <div className="w-5/12 border-b-2 border-b-black p-1 text-center text-lg">
           <p>Ordenar por:</p>
         </div>
-        <div className=" w-1/2">
-          <button className="peer cursor-default hover:text-blue-500">
+        <div className="flex w-3/4 items-center">
+          <button className="peer mt-1 cursor-default hover:border-b-2 hover:border-b-black hover:pb-2">
             {value}
           </button>
-          <div className="invisible mt-2 w-40 rounded-md border border-black bg-white hover:visible peer-focus:visible">
+          <div className="invisible mt-2 ml-2 w-1/3 rounded-md border border-black bg-white hover:visible peer-focus:visible">
             {/* <span className={spanSyles(value, "Más relevantes")}>
               Más relevantes
             </span> */}
-            <button
+            <span
               onClick={() => {
                 handleSort(
                   "Menor precio",
@@ -43,7 +43,7 @@ function Sort() {
               className={spanSyles(value, "Menor precio")}
             >
               Menor precio
-            </button>
+            </span>
             <span
               onClick={() => {
                 handleSort(
