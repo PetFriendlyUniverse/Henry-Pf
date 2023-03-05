6089dd77ca0c94d3e2b9cfedacfcc5253699b854
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-03-05 18:28:02 -0300

    Comments.jsx - (actualmente comment de review)

---
+++ client/src/pages/ecommerce/ProductDetail/components/Comments/Comments.jsx
@@ -0,0 +1,18 @@
+function Comments({ comments }) {
+  return (
+    <div className="mx-auto w-4/5 ">
+      {comments?.map((c, i) => (
+        <div
+          key={c.user + i}
+          className="m-4 flex w-3/5 flex-col rounded-md bg-white p-2"
+        >
+          <p>{c.user}</p>
+          <p>{c.comment}</p>
+          <span className="self-end text-gray-300">{c.date.split("T")[0]}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Comments;
