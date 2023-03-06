d72e36317633ed346a365ecfcf9f1c7391d2f8b2
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-03-06 20:18:32 -0300

    custom hook para traer todas las adopciones disponibles

---
+++ client/src/hooks/useGetAdopciones.js
@@ -0,0 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { getAdopcionesApi } from "../redux/features/adopcion/adopcionActions";
+
+function useGetAdopciones() {
+  const [loading, setLoading] = useState(true);
+  const { adopcion, adopcionPerPage, currentPage, province } = useSelector(
+    (state) => state.Services
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setLoading(true);
+    dispatch(getAdopcionesApi(currentPage, adopcionPerPage, province));
+  }, [currentPage, adopcionPerPage, province]);
+
+  useEffect(() => {
+    setLoading(false);
+    return () => {
+      setLoading(true);
+    };
+  }, [adopcion]);
+
+  return [loading, adopcion];
+}
+
+export default useGetAdopciones;
