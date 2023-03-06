bc19b794bfd2ca00224be40b5e1ac76d77575d5e
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-03-06 20:19:50 -0300

    actions de adopciones

--- client/src/redux/features/adopcion/adopcionActions.js
+++ client/src/redux/features/adopcion/adopcionActions.js
@@ -1,46 +1,22 @@
-import {
-  getAllAdopciones,
-  deletedAdopcion,
-  getFilters,
-  getFilters,
-  getAdopciones,
-  getAdopcionById,
-} from "./adopcionSlice";
+import { getAdopciones, getAdopcionById } from "./adopcionSlice";
 
 import axios from "axios";
-import { queryMarker } from "./helpers/queryMaker";
 
-const getAdopcionesApi =
-  (adopcionPerPage, currentPage, filters) => async (dispatch) => {
-    const query = queryMarker(adopcionPerPage, currentPage, filters);
-    const { data } = await axios.get(`/adopcion${query}/blog`);
+const getAdopcionesApi = (page, pq, province) => async (dispatch) => {
+  if (!province) {
+    const { data } = await axios.get(`/adopcion?page=${page}&pq=${pq}`);
     return dispatch(getAdopciones(data));
-  };
-
-const getFiltersApi = () => async (dispatch) => {
-  const { data } = await axios.get("adopcion/blog/filters");
-  return dispatch(getFilters(data));
+  } else {
+    const { data } = await axios.get(
+      `/adopcion?page=${page}&pq=${pq}&province=${province}`
+    );
+    return dispatch(getAdopciones(data));
+  }
 };
 
 const getAdopcionesIdApi = (id) => async (dispatch) => {
-  const { data } = await axios.get(`/adopcion/blog/${id}`);
+  const { data } = await axios.get(`/adopcion/${id}`);
   return dispatch(getAdopcionById(data));
 };
 
-const deletedAdopcionIdApi = (id) => async (dispatch) => {
-  const { data } = await axios.get(`/adopcion/blog/${id}`);
-  return dispatch(deletedAdopcion(data));
-};
-
-const getAllAdopcionesApi = () => async (dispatch) => {
-  const { data } = await axios.get("/adopcion");
-  return dispatch(getAllAdopciones(data));
-};
-
-export {
-  getAdopcionesApi,
-  getFiltersApi,
-  getAdopcionesIdApi,
-  deletedAdopcionIdApi,
-  getAllAdopcionesApi,
-};
+export { getAdopcionesApi, getAdopcionesIdApi };
