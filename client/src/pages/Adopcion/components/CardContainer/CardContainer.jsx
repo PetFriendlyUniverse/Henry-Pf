f88a2e33a8ffcd678149e45db87d59f49dc6ba9c
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-03-07 15:39:12 -0300

    responsive para sector adopciones en cascada con todo su interior

--- client/src/pages/Adopcion/components/CardContainer/CardContainer.jsx
+++ client/src/pages/Adopcion/components/CardContainer/CardContainer.jsx
@@ -11,7 +11,7 @@ function CardContainer() {
   }
   if (loading)
     return (
-      <div className="mx-auto flex w-full flex-wrap items-center justify-center justify-items-center gap-7 sm:w-4/5  md:max-w-2xl lg:m-0 lg:min-h-fit  lg:w-full   lg:gap-x-5 lg:gap-y-6   xl:gap-x-2  2xl:max-w-7xl  2xl:gap-x-10 2xl:gap-y-12  ">
+      <div className="mx-auto flex w-full  flex-wrap items-center justify-center justify-items-center gap-7 md:max-w-2xl lg:m-0 lg:min-h-fit  lg:w-full   lg:gap-x-5 lg:gap-y-6   xl:gap-x-2  2xl:max-w-7xl  2xl:gap-x-10 2xl:gap-y-12  ">
         {skeletonMap.map((i) => (
           <CardSkeleton key={"cs" + i} />
         ))}
@@ -19,7 +19,7 @@ function CardContainer() {
     );
   else {
     return (
-      <div className="mx-auto flex h-full w-full  flex-wrap  items-center justify-center justify-items-center gap-7 border-2 border-black ">
+      <div className=" flex h-full w-full flex-wrap  items-center justify-center justify-items-center gap-2 lg:gap-20 lg:px-10">
         {adoption?.length === 0 ? (
           <div className="min-h-[50vh]">
             <h2>No se encotraron coincidencias con los datos solicitados</h2>
