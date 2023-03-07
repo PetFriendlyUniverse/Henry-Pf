449bc73bdaae55d3325dcf9ff51dca6715135fbb
Benja <benyamin0714@gmail.com>
2023-03-07 15:16:51 -0300

    Benja - Scroll smooth y paginations

--- client/src/components/Paginator/Paginator.jsx
+++ client/src/components/Paginator/Paginator.jsx
@@ -18,6 +18,7 @@ function Paginator() {
 
   const changePage = ({ selected }) => {
     dispatch(setCurrentPage(selected + 1));
+    window.scrollTo(0, 0);
   };
 
   return (
@@ -97,10 +98,10 @@ function Paginator() {
 
       <CardContainer />
       <div className="relative mt-4 flex w-9/12 flex-col items-center justify-center gap-5 sm:w-96  2xl:flex-row">
-        <div className="w-full">
+        <div className="w-3/4">
           <Paginate
             pageCount={totalPages}
-            marginPagesDisplayed={0}
+            marginPagesDisplayed={1}
             onPageChange={changePage}
             forcePage={currentPage - 1}
             containerClassName={
