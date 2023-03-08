7fb90e155f5958c3aa54c4884093f124a07fc377
Benja <benyamin0714@gmail.com>
2023-03-07 21:04:56 -0300

    Benja - Mail confirmacion compra

--- api/src/handler/tokenHandler.js
+++ api/src/handler/tokenHandler.js
@@ -1,7 +1,9 @@
 const { MP_TOKEN } = process.env;
 const axios = require("axios");
+const { userBuyMail } = require("../controllers/mailController");
+
 const getTokenHandler = async (req, res) => {
-  const { merchantOrder } = req.query;
+  const { merchantOrder, id } = req.query;
   try {
     const { data } = await axios.get(
       `https://api.mercadopago.com/merchant_orders/${merchantOrder}`,
@@ -11,7 +13,8 @@ const getTokenHandler = async (req, res) => {
         },
       }
     );
-
+    console.log(id);
+    await userBuyMail(id);
     return res.status(200).json(data);
   } catch (error) {
     res.status(500).send(error.message);
