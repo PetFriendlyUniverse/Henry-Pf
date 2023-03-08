23a5df9acad9aba1ccf1a5d67cf4c75019b31f06
Federico Almeida <96883646+FedericoA15@users.noreply.github.com>
2023-03-08 03:11:25 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -24,8 +24,19 @@ const {
 // );
 
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< CONFIG PARA TODO
-const sequelize = new Sequelize(
-     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+const sequelize = DATABASE
+  ? new Sequelize(DATABASE, USERNAME, PASSWORD, {
+      host: HOST,
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(
+      `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
       {
         logging: false, // set to console.log to see the raw SQL queries
         native: false, // lets Sequelize know we can use pg-native for ~30% more speed
