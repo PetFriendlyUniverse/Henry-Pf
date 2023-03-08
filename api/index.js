c043c95467fbcec09e06e185844f7b7b11ab83e9
Federico Almeida <96883646+FedericoA15@users.noreply.github.com>
2023-03-08 02:22:59 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const { PORT, FORCE_TRUE } = process.env;
+const { PORT } = process.env;
 
-const port = PORT || 3001;
 
-conn.sync().then(() => {
-  server.listen(port, () => {
-    console.log("%s listening at 3001");
+conn.sync({ alter: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log("%s listening at 3001",PORT);
   });
 });
