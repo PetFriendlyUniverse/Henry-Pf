84878783dd98982d65db4f331d8fe5918f462157
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-03-09 13:52:54 -0300

    ultimos cambios pre presentacion

--- client/src/components/CardStoreIdContainer/CardStoreIdContainer.jsx
+++ client/src/components/CardStoreIdContainer/CardStoreIdContainer.jsx
@@ -7,7 +7,7 @@ function CardStoreIdContainer({ id }) {
   return (
     <div
       className={` max-h[800] flex min-h-[350px] min-w-full flex-wrap justify-center gap-8 overflow-x-hidden ${
-        products.length > 0 && overflow - y - scroll
+        products.length > 0 && "overflow-y-scroll"
       }  border-2 border-gray-50 bg-[#fff] px-3 pb-2`}
     >
       {products?.map((product) => {
