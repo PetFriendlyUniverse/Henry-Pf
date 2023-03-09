303fda3bde7cf2a0dfb48d2fa9273be715c24002
Benja <benyamin0714@gmail.com>
2023-03-09 00:53:41 -0300

    estilo formcreateDaycare

--- client/src/pages/Services/Forms/FormCreateDaycare/FormCreateDaycare.jsx
+++ client/src/pages/Services/Forms/FormCreateDaycare/FormCreateDaycare.jsx
@@ -168,7 +168,7 @@ function FormCreateDaycare() {
                 Nombre de la guarderia
               </label>
               {errors.name && (
-                <span className="text-red-500">{errors.name}</span>
+                <span className="text-xs text-red-500">{errors.name}</span>
               )}
             </div>
             {/* corre electronico */}
@@ -186,7 +186,7 @@ function FormCreateDaycare() {
                 Correo Electronico
               </label>
               {errors.mail && (
-                <span className="text-red-500">{errors.mail}</span>
+                <span className="text-xs text-red-500">{errors.mail}</span>
               )}
             </div>
 
@@ -243,7 +243,9 @@ function FormCreateDaycare() {
                   Cod. Postal
                 </label>
                 {errors.zip_code && (
-                  <span className="text-red-500">{errors.zip_code}</span>
+                  <span className="text-xs text-red-500">
+                    {errors.zip_code}
+                  </span>
                 )}
               </div>
               <div className="group relative z-0 mx-4 mb-6 h-11 w-full">
@@ -260,7 +262,9 @@ function FormCreateDaycare() {
                   Calle
                 </label>
                 {errors.street_name && (
-                  <span className="text-red-500">{errors.street_name}</span>
+                  <span className="text-xs text-red-500">
+                    {errors.street_name}
+                  </span>
                 )}
               </div>
               <div className="group relative z-0 mb-6 h-11 w-full">
@@ -277,7 +281,9 @@ function FormCreateDaycare() {
                   Numeración
                 </label>
                 {errors.street_number && (
-                  <span className="text-red-500">{errors.street_number}</span>
+                  <span className="text-xs text-red-500">
+                    {errors.street_number}
+                  </span>
                 )}
               </div>
             </div>
@@ -296,7 +302,9 @@ function FormCreateDaycare() {
                   Precio por Hora
                 </label>
                 {errors.price_hour && (
-                  <span className="text-red-500">{errors.price_hour}</span>
+                  <span className="text-xs text-red-500">
+                    {errors.price_hour}
+                  </span>
                 )}
               </div>
               <div className="group relative z-0 mb-6 h-11 w-full">
@@ -313,7 +321,9 @@ function FormCreateDaycare() {
                   Precio por Dia
                 </label>
                 {errors.price_day && (
-                  <span className="text-red-500">{errors.price_day}</span>
+                  <span className="text-xs text-red-500">
+                    {errors.price_day}
+                  </span>
                 )}
               </div>
             </div>
@@ -331,7 +341,7 @@ function FormCreateDaycare() {
                 Descripción:
               </label>
               {errors.description && (
-                <span className="absolute -bottom-6 text-red-500">
+                <span className="absolute -bottom-6 text-xs text-red-500">
                   {errors.description}
                 </span>
               )}
@@ -349,7 +359,7 @@ function FormCreateDaycare() {
                   ))}
               </Carousel>
             </div>
-            <div className="h-1/2 p-8 px-8 ">
+            <div className="h-2/5 p-6 px-8 ">
               <div className="group relative z-0 mb-14 flex h-11 w-full ">
                 <input
                   type="file"
@@ -375,7 +385,9 @@ function FormCreateDaycare() {
                     Cod. de area
                   </label>
                   {errors.area_code && (
-                    <span className="text-red-500">{errors.area_code}</span>
+                    <span className="text-xs text-red-500">
+                      {errors.area_code}
+                    </span>
                   )}
                 </div>
                 {/* telefono */}
@@ -393,7 +405,9 @@ function FormCreateDaycare() {
                     Teléfono
                   </label>
                   {errors.number && (
-                    <span className="text-red-500">{errors.number}</span>
+                    <span className="text-xs text-red-500">
+                      {errors.number}
+                    </span>
                   )}
                 </div>
               </div>
