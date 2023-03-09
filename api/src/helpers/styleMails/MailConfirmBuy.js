4376afc3b315aed4845a9aea72c043e71e0865a0
Benja <benyamin0714@gmail.com>
2023-03-08 22:04:23 -0300

    Estilo de mail confirmacion de compra

---
+++ api/src/helpers/styleMails/MailConfirmBuy.js
@@ -0,0 +1,222 @@
+const MailConfirmBuy = `
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta name="x-apple-disable-message-reformatting">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="telephone=no" name="format-detection">
+    <title></title>
+    <!--[if (mso 16)]>
+    <style type="text/css">
+    a {text-decoration: none;}
+    </style>
+    <![endif]-->
+    <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]-->
+    <!--[if gte mso 9]>
+<xml>
+    <o:OfficeDocumentSettings>
+    <o:AllowPNG></o:AllowPNG>
+    <o:PixelsPerInch>96</o:PixelsPerInch>
+    </o:OfficeDocumentSettings>
+</xml>
+<![endif]-->
+</head>
+
+<body>
+    <div class="es-wrapper-color">
+        <!--[if gte mso 9]>
+			<v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
+				<v:fill type="tile" color="#ffffff"></v:fill>
+			</v:background>
+		<![endif]-->
+        <table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0">
+            <tbody>
+                <tr>
+                    <td class="esd-email-paddings" valign="top">
+                        <table cellpadding="0" cellspacing="0" class="es-header esd-header-popover" align="center">
+                            <tbody>
+                                <tr>
+                                    <td class="esd-stripe" align="center" esd-custom-block-id="89006">
+                                        <table class="es-header-body" width="450" cellspacing="0" cellpadding="0" bgcolor="rgba(0, 0, 0, 0)" align="center">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="esd-structure es-p20t es-p20b es-p20r es-p20l" style="background-position: center bottom;" esd-custom-block-id="25140" align="left">
+                                                        <table width="100%" cellspacing="0" cellpadding="0">
+                                                            <tbody>
+                                                                <tr>
+                                                                    <td class="esd-container-frame" width="410" valign="top" align="center">
+                                                                        <table width="100%" cellspacing="0" cellpadding="0">
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                    <td align="center" class="esd-block-image" style="font-size: 0px;"><a target="_blank"><img class="adapt-img" src="https://mfwfzr.stripocdn.email/content/guids/d517c4c1-96b0-4cde-b5d8-bab3d8101842/images/recurso_1.png" alt style="display: block;" width="200"></a></td>
+                                                                                </tr>
+                                                                            </tbody>
+                                                                        </table>
+                                                                    </td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <table cellpadding="0" cellspacing="0" class="es-header" align="center">
+                            <tbody>
+                                <tr>
+                                    <td class="es-adaptive esd-stripe" style="background-color: #1376c8;" esd-custom-block-id="25141" bgcolor="#1376c8" align="center">
+                                        <table class="es-header-body" style="background-color: transparent;" width="450" cellspacing="0" cellpadding="0" bgcolor="transparent" align="center">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="esd-structure es-p15t es-p15b es-p15r es-p15l" style="background-position: center top;" align="left">
+                                                        <table width="100%" cellspacing="0" cellpadding="0">
+                                                            <tbody>
+                                                                <tr>
+                                                                    <td class="esd-container-frame" width="420" valign="top" align="center">
+                                                                        <table width="100%" cellspacing="0" cellpadding="0">
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                    <td class="esd-block-menu" esd-img-prev-h="16" esd-img-prev-w="16">
+                                                                                        <table class="es-menu" width="100%" cellspacing="0" cellpadding="0">
+                                                                                            <tbody>
+                                                                                                <tr class="links">
+                                                                                                    <td class="es-p10t es-p10b es-p5r es-p5l" style="padding-bottom: 0px; padding-top: 0px; " width="100%" bgcolor="transparent" align="center"><a target="_blank" style="color: #ffffff; font-weight: bold; font-family: arial,'helvetica neue',helvetica,sans-serif;" href="https://petfriendlyuniverse.vercel.app/">PET FRIENDLY UNIVERSE</a></td>
+                                                                                                </tr>
+                                                                                            </tbody>
+                                                                                        </table>
+                                                                                    </td>
+                                                                                </tr>
+                                                                            </tbody>
+                                                                        </table>
+                                                                    </td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <table class="es-content" cellspacing="0" cellpadding="0" align="center">
+                            <tbody>
+                                <tr>
+                                    <td class="esd-stripe" esd-custom-block-id="25142" style="background-color: #f3f3f3;" bgcolor="#f3f3f3" align="center">
+                                        <table class="es-content-body" style="background-color: transparent;" width="450" cellspacing="0" cellpadding="0" bgcolor="transparent" align="center">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="esd-structure es-p25t es-p5b es-p20r es-p20l" style="background-position: right bottom;" align="left">
+                                                        <table width="100%" cellspacing="0" cellpadding="0">
+                                                            <tbody>
+                                                                <tr>
+                                                                    <td class="esd-container-frame" width="410" valign="top" align="center">
+                                                                        <table width="100%" cellspacing="0" cellpadding="0">
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                    <td class="esd-block-text es-p20t es-p10b" align="center">
+                                                                                        <h1>Gracias por tu compra!!</h1>
+                                                                                    </td>
+                                                                                </tr>
+                                                                                <tr>
+                                                                                    <td class="esd-block-text es-m-txt-c es-p10t es-p15b" align="center">
+                                                                                        <p>Gracias a tu compra en nuestra pagina ayudas a las ONG asociadas para que ellas puedan seguir rescatando muchos cachorros y conseguirles un hogar calido lleno de amor.&nbsp;</p>
+                                                                                    </td>
+                                                                                </tr>
+                                                                                <tr>
+                                                                                    <td class="esd-block-text es-m-txt-c es-p20b" align="center">
+                                                                                        <p>Muchas gracias por la compra en nuestra pagina!!</p>
+                                                                                    </td>
+                                                                                </tr>
+                                                                                <tr>
+                                                                                    <td class="esd-block-button es-p10r es-p10l" align="center"><span class="es-button-border" style="background: #1376c8 none repeat scroll 0% 0%;"><a href="https://petfriendlyuniverse.vercel.app/shop" class="es-button" target="_blank" style="font-weight: normal; font-family: 'trebuchet ms', 'lucida grande', 'lucida sans unicode', 'lucida sans', tahoma, sans-serif; background: #1376c8 none repeat scroll 0% 0%; border-color: #1376c8; color: #ffffff; font-size: 18px">Hacer otra compra</a></span></td>
+                                                                                </tr>
+                                                                            </tbody>
+                                                                        </table>
+                                                                    </td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="esd-structure es-p20r es-p20l" style="background-position: right bottom;" align="left">
+                                                        <table width="100%" cellspacing="0" cellpadding="0">
+                                                            <tbody>
+                                                                <tr>
+                                                                    <td class="esd-container-frame" width="410" valign="top" align="center">
+                                                                        <table width="100%" cellspacing="0" cellpadding="0">
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                    <td class="esd-block-image" align="left" style="font-size:0"><a target="_blank"><img class="adapt-img" src="https://mfwfzr.stripocdn.email/content/guids/CABINET_a20d5d34e9dff5d8e26f5eda22682851/images/21881547721107368.jpg" alt style="display: block;" width="410"></a></td>
+                                                                                </tr>
+                                                                            </tbody>
+                                                                        </table>
+                                                                    </td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <table cellpadding="0" cellspacing="0" class="es-content esd-footer-popover" align="center">
+                            <tbody>
+                                <tr>
+                                    <td class="esd-stripe" esd-custom-block-id="25154" align="center">
+                                        <table class="es-content-body" width="450" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="esd-structure es-p20t es-p20b es-p20r es-p20l" style="background-position: center bottom;" align="left">
+                                                        <table width="100%" cellspacing="0" cellpadding="0">
+                                                            <tbody>
+                                                                <tr>
+                                                                    <td class="esd-container-frame" width="410" valign="top" align="center">
+                                                                        <table width="100%" cellspacing="0" cellpadding="0">
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                    <td align="center" class="esd-block-text es-infoblock">
+                                                                                        <p>Has recibido este mail para confirmar que tu compra se realizo con exito.<br></p>
+                                                                                    </td>
+                                                                                </tr>
+                                                                                <tr>
+                                                                                    <td class="esd-block-text es-p10t es-infoblock" align="center">
+                                                                                        <p>© Pet Friendly Universe, Inc.</p>
+                                                                                    </td>
+                                                                                </tr>
+                                                                            </tbody>
+                                                                        </table>
+                                                                    </td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
+`;
+
+module.exports = { MailConfirmBuy };
