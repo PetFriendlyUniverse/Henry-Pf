1e155dd363519f4c5d26dcc469726c1d9d4200b3
AgustinChavero <agustin199orellano@gmail.com>
2023-03-08 23:00:13 -0300

    Estilos y render de DetailDaycare

--- client/src/pages/Services/Detail/DetailDaycare/DetailDaycare.jsx
+++ client/src/pages/Services/Detail/DetailDaycare/DetailDaycare.jsx
@@ -2,7 +2,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Detail from "./Components/Detail/Detail";
-import Description from "./Components/Description/Description";
 import { getDaycareApi } from "../../../../redux/features/services/servicesActions";
 
 function ServicesDetail() {
@@ -30,9 +29,6 @@ function ServicesDetail() {
             street_number={daycare?.street_number}
           />
         </div>
-        {/* <div>
-          <Description description={daycare?.description} />
-        </div> */}
       </div>
     </div>
   );
