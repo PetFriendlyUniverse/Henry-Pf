3c775c53110671156676759bae8f7daac09afa3b
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-03-08 23:15:51 -0300

    estilos

--- client/src/components/Button/LinkButton.jsx
+++ client/src/components/Button/LinkButton.jsx
@@ -1,11 +1,13 @@
 function LinkButton({ component, onClick }) {
   return (
-    <button
-      onClick={onClick}
-      className="mx-3 rounded-md bg-blue-900 px-3 py-2 text-center text-sm font-medium text-white shadow-md shadow-black transition-all duration-200 hover:bg-blueHover active:translate-y-1"
-    >
-      {component}
-    </button>
+    <div className="flex w-full justify-center">
+      <button
+        onClick={onClick}
+        className="mx-3 w-1/2 justify-center rounded-md bg-blue-900 px-3 py-2 text-center text-sm font-medium text-white shadow-md shadow-black transition-all duration-200 hover:bg-blueHover active:translate-y-1 "
+      >
+        {component}
+      </button>
+    </div>
   );
 }
 
