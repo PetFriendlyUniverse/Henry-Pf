06bdbe969a62a290374bfa6e8d2743fa4643fefd
Federico Almeida <96883646+FedericoA15@users.noreply.github.com>
2023-03-08 22:48:33 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const { conn } = require("./src/db.js");
 const { PORT } = process.env;
 
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   server.listen(PORT, () => {
     console.log("%s listening at 3001");
   });
