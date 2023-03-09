0f94d384652168f5b92ff26a2a0365539ab4bc2b
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-03-09 01:39:37 -0300

    estilado PhotoStore

--- client/src/pages/ecommerce/Profile/components/Store/PhotoStore.jsx
+++ client/src/pages/ecommerce/Profile/components/Store/PhotoStore.jsx
@@ -3,7 +3,7 @@ import photo from "../../../../../assets/general/profile.svg";
 function PhotoStore({ name, img }) {
   return (
     <div className="flex flex-col items-center justify-center pt-2 md:justify-between md:px-11">
-      <div className="mb-4 w-56 sm:mb-0 sm:w-72 md:w-20">
+      <div className="mb-4 w-32 sm:mb-0">
         <img src={img || photo} alt="profile" className="rounded-full" />
       </div>
       <div className="mx-0 text-center sm:text-left">
