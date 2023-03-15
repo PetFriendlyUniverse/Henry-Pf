bbeffaba4204a60c3b5ab92171728cad3a04400c
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-03-14 21:12:12 -0300

    estilos detail daycare

--- client/src/pages/Services/Detail/DetailDaycare/Components/Detail/Detail.jsx
+++ client/src/pages/Services/Detail/DetailDaycare/Components/Detail/Detail.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { getDaycareApi } from "../../../../../../redux/features/services/servicesActions";
+import { Carousel } from "flowbite-react";
 
 function Detail({
   name,
@@ -27,56 +28,67 @@ function Detail({
   }, []);
 
   return (
-    <div className="flex justify-center text-black">
-      <div className="shadow-2xl">
-        <div className="max-w-[1000px] rounded-t-lg border-t-2 border-r-2 border-l-2 border-russianviolet bg-zinc-100 lg:flex">
-          <div className="flex justify-center">
-            <img src={`${img}`} alt={`${img}`} className=" rounded-tl-md" />
+    <div className="flex flex-col gap-8">
+      <div className="container mx-auto min-h-[70vh] max-w-sm overflow-hidden rounded-2xl border border-black lg:max-w-xl">
+        <div className="grid h-14 place-content-center bg-zinc-400">
+          <p className="text-base font-bold tracking-[1px] md:text-2xl lg:text-3xl">
+            {name}
+          </p>
+        </div>
+        <div className="flex h-full flex-col">
+          <div className="flex h-60 items-center justify-center">
+            <Carousel className="lg:w-4/5">
+              <img src={img} alt={img} className="h-full" />
+            </Carousel>
           </div>
-          <div className="border-b-2">
-            <div className="flex justify-center border-b-2 bg-zinc-200 py-4 lg:w-[500px] lg:rounded-tr-md">
-              <p className="font-bold md:text-2xl lg:text-3xl">{`${name}`}</p>
-            </div>
-            <div className="px-10 py-2">
-              <p className="font-medium">
-                Precio por hora: ${`${price_hour}`} (c/u)
-              </p>
-              {price_day ? (
-                <p className="font-medium">
-                  Precio por dia: {`${price_day}`} (c/u)
-                </p>
-              ) : null}
-            </div>
-            <div className="px-10 py-2">
-              <p className="py-4 font-medium">
-                Provincia:
+          <div className="mx-auto flex h-full w-4/5 flex-col justify-around gap-4 p-3">
+            {/* ubicacion */}
+            <div className="flex flex-col gap-1">
+              <p className="">
+                <span className="font-bold">Provincia:</span>
                 {province ? ` ${province} ` : " No disponible"}
               </p>
-              <p className="py-4 font-medium">
-                Localidad:
+              <p className="">
+                <span className="font-bold">Localidad:</span>
                 {locality ? ` ${locality}` : " No disponible"}
               </p>
               {street_name && (
                 <p>Dirección: {`${street_name} - ${street_number}`}</p>
               )}
             </div>
-            <div className="px-10 py-2">
-              <h6 className="pb-5 font-medium">Contacto:</h6>
-              <div className="ml-20 font-medium sm:ml-40 md:ml-44 lg:ml-0">
-                {area_code ? (
-                  <p>{`${area_code} - ${number}`}</p>
-                ) : (
-                  "Telefono no disponible"
-                )}
-                {mail && <p>{`${mail}`}</p>}
+            {/* contacto */}
+            <div className="flex flex-col gap-1">
+              <h6 className="text-center font-bold uppercase">Contacto:</h6>
+              <div className="flex flex-col gap-1">
+                <div>
+                  <span className="font-bold">Teléfono: </span>
+                  {area_code ? (
+                    <p>{`${area_code} - ${number}`}</p>
+                  ) : (
+                    <p>No disponible</p>
+                  )}
+                </div>
+                <div>
+                  <span className="font-bold">Email: </span>
+                  {mail && <p>{`${mail}`}</p>}
+                </div>
               </div>
+            </div>
+            {/* precios */}
+            <div className="flex flex-col gap-3">
+              <p className="rounded-lg bg-pastelgreen py-1 px-2 font-medium shadow-xl shadow-black">
+                Precio por hora: ${`${price_hour}`} (c/u)
+              </p>
+              {price_day ? (
+                <p className="rounded-lg bg-pastelgreen py-1 px-2 font-medium shadow-xl shadow-black">
+                  Precio por dia: {`${price_day}`} (c/u)
+                </p>
+              ) : null}
             </div>
           </div>
         </div>
-        <div>
-          <Description />
-        </div>
       </div>
+      <Description />
     </div>
   );
 }
